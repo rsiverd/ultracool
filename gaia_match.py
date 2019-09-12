42051f51213e7238f6d9e4d2a2be58bfcdda284f
Rob Siverd <robert.siverd@gmail.com>
2019-09-11 16:54:13 -1000

    Big speedup achieved by performing separation checks using numpy arrays instead of DataFrames. Increased __version__ to 0.2.0.

--- gaia_match.py
+++ gaia_match.py
@@ -5,13 +5,13 @@
 #
 # Rob Siverd
 # Created:       2019-09-09
-# Last modified: 2019-09-09
+# Last modified: 2019-09-11
 #--------------------------------------------------------------------------
 #**************************************************************************
 #--------------------------------------------------------------------------
 
 ## Current version:
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 ## Python version-agnostic module reloading:
 try:
@@ -84,10 +84,17 @@ class GaiaMatch(object):
         self._srcdata = None
         self._ra_key = 'ra'
         self._de_key = 'dec'
+        self._angdev = 180.0
         return
 
+    @staticmethod
+    def _min_and_idx(a):
+        """Return minimum value of array and index position of that value."""
+        idx = np.argmin(a)
+        return (a[idx], idx)
+
     def _have_sources(self):
-        return True if self._srcdata else False
+        return True if isinstance(self._srcdata, pd.DataFrame) else False
 
     def load_sources_csv(self, filename):
         # error if file not present:
@@ -103,13 +110,22 @@ class GaiaMatch(object):
             raise
 
         # spread estimate:
+        dde = self._srcdata[self._de_key].values
+        self._angdev = np.median(np.abs(dde - np.median(dde)))
         return
 
-    def nearest_star(self, ra, dec):
+    def nearest_star(self, ra, dec, toler=None):
         """
         Identify the source closest to the given position. RA and DE must
         be given in decimal degrees. 
-        
+
+        Params:
+        -------
+        ra      -- R.A. in decimal degrees
+        dec     -- Dec in decimal degrees
+        toler   -- [optional] maximum matching distance. If not specified,
+                        the auto-computed angdev is used for this.
+ 
         Returns:
         --------
         sep     -- distance in DEGREES to nearest match
@@ -117,7 +133,44 @@ class GaiaMatch(object):
         """
         if not self._have_sources():
             logging.error("No sources loaded. Load data and try again.")
-        return
+
+        # Working coordinate arrays:
+        sra = self._srcdata[self._ra_key].values
+        sde = self._srcdata[self._de_key].values
+
+        ## Initial cut in Dec:
+        #use_tol = toler if toler else self._angdev
+        #decnear = (np.abs(self._srcdata[self._de_key] - dec) <= use_tol)
+        #subset  = self._srcdata[decnear].copy()
+
+        if toler:
+            use_tol = toler
+            #decnear = (np.abs(self._srcdata[self._de_key] - dec) <= use_tol)
+            decnear = (np.abs(sde - dec) <= use_tol)
+            wrk_idx = decnear.nonzero()[0]
+            #subset  = self._srcdata[decnear].copy()
+            #sys.stderr.write("nearidx: %s\n" % str(nearidx))
+            sub_ra  = sra[wrk_idx]
+            sub_de  = sde[wrk_idx]
+            #sys.stderr.write("winner: %d\n" % winner)
+        else:
+            #subset  = self._srcdata.copy()
+            wrk_idx = np.arange(len(sra))
+            sub_ra  = sra
+            sub_de  = sde
+
+        # Proper angular separation:
+        sep_deg = angle.dAngSep(ra, dec, sub_ra, sub_de)
+        hit_idx = np.argmin(sep_deg)      # best match index in subset
+        origidx = wrk_idx[hit_idx]        # best match index in dataset
+        match   = self._srcdata.iloc[[origidx]].copy()
+        match['dist'] = sep_deg[hit_idx]
+        return match
+        ## Full calculation on remaining objects:
+        #subset['dist'] = angle.dAngSep(ra, dec,
+        #        subset[self._ra_key], subset[self._de_key])
+        #return subset.iloc[[subset.dist.values.argmin()]]
+        ##return subset[subset.dist == subset.dist.min()]
 
 ##--------------------------------------------------------------------------##
 
