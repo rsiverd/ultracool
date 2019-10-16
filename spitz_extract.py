565bdbcfe78a52723c78ee7a47c5cee31854dd12
Rob Siverd <robert.siverd@gmail.com>
2019-10-15 15:17:25 -1000

    module seems to work as intended now

--- spitz_extract.py
+++ spitz_extract.py
@@ -56,31 +56,6 @@ except ImportError:
 
 ##--------------------------------------------------------------------------##
 
-## Home-brew robust statistics:
-#try:
-#    import robust_stats
-#    reload(robust_stats)
-#    rs = robust_stats
-#except ImportError:
-#    logger.error("module robust_stats not found!  Install and retry.")
-#    sys.stderr.write("\nError!  robust_stats module not found!\n"
-#           "Please install and try again ...\n\n")
-#    sys.exit(1)
-
-## FITS I/O:
-#try:
-#    import astropy.io.fits as pf
-#except ImportError:
-#    try:
-#       import pyfits as pf
-#    except ImportError:
-#        logger.error("No FITS I/O module found!"
-#                "Install either astropy.io.fits or pyfits and retry."))
-#        logger.error("No FITS I/O module found!")
-#        sys.stderr.write("\nError!  No FITS I/O module found!\n"
-#               "Install either astropy.io.fits or pyfits and try again!\n\n")
-#        sys.exit(1)
-
 ## Various from astropy:
 try:
 #    import astropy.io.ascii as aia
@@ -88,8 +63,6 @@ try:
 #    import astropy.table as apt
 #    import astropy.time as astt
     import astropy.wcs as awcs
-#    from astropy import coordinates as coord
-#    from astropy import units as uu
 except ImportError:
     logger.error("astropy module not found!  Install and retry.")
 #    sys.stderr.write("\nError: astropy module not found!\n")
@@ -118,9 +91,13 @@ class SpitzFind(object):
     def __init__(self):
         self._pse = easy_sep.EasySEP()
         self._pse.set_options(**_spitz_defaults)
+        # science image:
+        self._ipath = None
         self._idata = None
         self._ihdrs = None
         self._imwcs = None
+        # uncertainty image:
+        self._upath = None
         self._udata = None
         self._uhdrs = None
         self._have_err_image = False
@@ -143,9 +120,12 @@ class SpitzFind(object):
                 self._idata, self._ihdrs = self._get_data_and_header(ipath)
                 self._pse.set_image(self._idata, _docopy=False)
                 self._imwcs = awcs.WCS(self._ihdrs)
+                self._pse.set_imwcs(self._imwcs.all_pix2world)
+                self._ipath = ipath
             except:
                 logger.error("Failed to load file: %s" % ipath)
-                self._idata, self._ihdrs = None, None
+                self._ipath, self._idata, self._ihdrs = None, None, None
+
         # error image:
         if upath:
             logger.info("Loading error image %s" % upath)
@@ -153,9 +133,10 @@ class SpitzFind(object):
                 self._udata, self._uhdrs = self._get_data_and_header(upath)
                 self._have_err_image = True
                 self._pse.set_errs(self._udata, _docopy=False)
+                self._upath = upath
             except:
                 logger.error("Failed to load file: %s" % ipath)
-                self._udata, self._uhdrs = None, None
+                self._upath, self._udata, self._uhdrs = None, None, None
                 self._have_err_image = False
         return
 
@@ -166,256 +147,24 @@ class SpitzFind(object):
         return rdata.astype('float32'), rhdrs.copy(strip=True)
 
     # ----------------------------------------
-    def find_stars(self, thresh, rel_err):
+    def find_stars(self, thresh, keepall=False, use_err_img=True):
         """Driver routine for star extraction. Required inputs:
-        ipath   --  path to image for analysis
-        thresh  --  significance threshold for star extraction
-        upath   --  [optional] path to uncertainty image
+        thresh      --  significance threshold for star extraction
+        use_err_img --  False disables use of error-image
 
         Results are reported in an ExtendedCatalog container.
         """
+        _err_mode = use_err_img and self._have_err_image
+        self._pse.analyze(thresh, rel_err=_err_mode)
+        dataset = self._pse.allobjs if keepall else self._pse.useobjs
 
-
-##--------------------------------------------------------------------------##
-## Quick ASCII I/O:
-#data_file = 'data.txt'
-
-#gftkw = {'encoding':None} if (_have_np_vers >= 1.14) else {}
-#gftkw.update({'names':True, 'autostrip':True})
-#gftkw.update({'delimiter':'|', 'comments':'%0%0%0%0'})
-#gftkw.update({'loose':True, 'invalid_raise':False})
-#all_data = np.genfromtxt(data_file, dtype=None, **gftkw)
-#all_data = aia.read(data_file)
-#all_data = pd.read_csv(data_file)
-#all_data = pd.read_table(data_file, delim_whitespace=True)
-#all_data = pd.read_table(data_file, sep='|')
-#fields = all_data.dtype.names
-#if not fields:
-#    x = all_data[:, 0]
-#    y = all_data[:, 1]
-#else:
-#    x = all_data[fields[0]]
-#    y = all_data[fields[1]]
+        result = ec.ExtendedCatalog(data=dataset,
+                name=os.path.basename(self._ipath), header=self._ihdrs,
+                uname=os.path.basename(self._upath), uheader=self._uhdrs)
+        return result
 
 ##--------------------------------------------------------------------------##
-## Timestamp modification:
-#def time_warp(jdutc, jd_offset, scale):
-#    return (jdutc - jd_offset) * scale
-
-## Self-consistent time-modification for plotting:
-#tfudge = partial(time_warp, jd_offset=tstart.jd, scale=24.0)    # relative hrs
-#tfudge = partial(time_warp, jd_offset=tstart.jd, scale=1440.0)  # relative min
-
 ##--------------------------------------------------------------------------##
-## Quick FITS I/O:
-#data_file = 'image.fits'
-#img_vals = pf.getdata(data_file)
-#hdr_keys = pf.getheader(data_file)
-#img_vals, hdr_keys = pf.getdata(data_file, header=True)
-#img_vals, hdr_keys = pf.getdata(data_file, header=True, uint=True) # USHORT
-#img_vals, hdr_keys = fitsio.read(data_file, header=True)
-
-#date_obs = hdr_keys['DATE-OBS']
-#site_lat = hdr_keys['LATITUDE']
-#site_lon = hdr_keys['LONGITUD']
-
-## Initialize time:
-#img_time = astt.Time(hdr_keys['DATE-OBS'], scale='utc', format='isot')
-#img_time += astt.TimeDelta(0.5 * hdr_keys['EXPTIME'], format='sec')
-#jd_image = img_time.jd
-
-## Initialize location:
-#observer = ephem.Observer()
-#observer.lat = np.radians(site_lat)
-#observer.lon = np.radians(site_lon)
-#observer.date = img_time.datetime
-
-#pf.writeto('new.fits', img_vals)
-#qsave('new.fits', img_vals)
-#qsave('new.fits', img_vals, header=hdr_keys)
-
-## Star extraction:
-#pse.set_image(img_vals, gain=3.6)
-#objlist = pse.analyze(sigthresh=5.0)
-
-##--------------------------------------------------------------------------##
-## Misc:
-#def log_10_product(x, pos):
-#   """The two args are the value and tick position.
-#   Label ticks with the product of the exponentiation."""
-#   return '%.2f' % (x)  # floating-point
-#
-#formatter = plt.FuncFormatter(log_10_product) # wrap function for use
-
-## Convenient, percentile-based plot limits:
-#def nice_limits(vec, pctiles=[1,99], pad=1.2):
-#    ends = np.percentile(vec[~np.isnan(vec)], pctiles)
-#    middle = np.average(ends)
-#    return (middle + pad * (ends - middle))
-
-## Convenient plot limits for datetime/astropy.Time content:
-#def nice_time_limits(tvec, buffer=0.05):
-#    lower = tvec.min()
-#    upper = tvec.max()
-#    ndays = upper - lower
-#    return ((lower - 0.05*ndays).datetime, (upper + 0.05*ndays).datetime)
-
-## Convenient limits for datetime objects:
-#def dt_limits(vec, pad=0.1):
-#    tstart, tstop = vec.min(), vec.max()
-#    trange = (tstop - tstart).total_seconds()
-#    tpad = dt.timedelta(seconds=pad*trange)
-#    return (tstart - tpad, tstop + tpad)
-
-##--------------------------------------------------------------------------##
-## Solve prep:
-#ny, nx = img_vals.shape
-#x_list = (0.5 + np.arange(nx)) / nx - 0.5            # relative (centered)
-#y_list = (0.5 + np.arange(ny)) / ny - 0.5            # relative (centered)
-#xx, yy = np.meshgrid(x_list, y_list)                 # relative (centered)
-#xx, yy = np.meshgrid(nx*x_list, ny*y_list)           # absolute (centered)
-#xx, yy = np.meshgrid(np.arange(nx), np.arange(ny))   # absolute
-#yy, xx = np.meshgrid(np.arange(ny), np.arange(nx), indexing='ij') # absolute
-#yy, xx = np.nonzero(np.ones_like(img_vals))          # absolute
-#yy, xx = np.mgrid[0:ny,   0:nx].astype('uint16')     # absolute (array)
-#yy, xx = np.mgrid[1:ny+1, 1:nx+1].astype('uint16')   # absolute (pixel)
-
-## 1-D vectors:
-#x_pix, y_pix, ivals = xx.flatten(), yy.flatten(), img_vals.flatten()
-#w_vec = np.ones_like(ivals)            # start with uniform weights
-#design_matrix = np.column_stack((np.ones(x_pix.size), x_pix, y_pix))
-
-## Image fitting (statsmodels etc.):
-#data = sm.datasets.stackloss.load()
-#ols_res = sm.OLS(ivals, design_matrix).fit()
-#rlm_res = sm.RLM(ivals, design_matrix).fit()
-#rlm_model = sm.RLM(ivals, design_matrix, M=sm.robust.norms.HuberT())
-#rlm_res = rlm_model.fit()
-#data = pd.DataFrame({'xpix':x_pix, 'ypix':y_pix})
-#rlm_model = sm.RLM.from_formula("ivals ~ xpix + ypix", data)
-
-##--------------------------------------------------------------------------##
-## Theil-Sen line-fitting (linear):
-#model = ts.linefit(xvals, yvals)
-#icept, slope = ts.linefit(xvals, yvals)
-
-## Theil-Sen line-fitting (loglog):
-#xvals, yvals = np.log10(original_xvals), np.log10(original_yvals)
-#xvals, yvals = np.log10(df['x'].values), np.log10(df['y'].values)
-#llmodel = ts.linefit(np.log10(xvals), np.log10(yvals))
-#icept, slope = ts.linefit(xvals, yvals)
-#fit_exponent = slope
-#fit_multiplier = 10**icept
-#bestfit_x = np.arange(5000)
-#bestfit_y = fit_multiplier * bestfit_x**fit_exponent
-
-## Log-log evaluator:
-#def loglog_eval(xvals, model):
-#    icept, slope = model
-#    return 10**icept * xvals**slope
-#def loglog_eval(xvals, icept, slope):
-#    return 10**icept * xvals**slope
-
-##--------------------------------------------------------------------------##
-## KDE:
-#kde_pnts, kde_vals = mk.go(data_vec)
-
-##--------------------------------------------------------------------------##
-## Vaex plotting:
-#ds = vaex.open('big_file.hdf5')
-#ds = vaex.from_arrays(x=x, y=y)     # load from arrays
-#ds = vaex.from_csv('mydata.csv')
-
-## Stats:
-#ds.mean("x"), ds.std("x"), ds.correlation("vx**2+vy**2+vz**2", "E")
-#ds.plot(....)
-#http://vaex.astro.rug.nl/latest/tutorial_ipython_notebook.html
-
-##--------------------------------------------------------------------------##
-## Plot config:
-
-# gridspec examples:
-# https://matplotlib.org/users/gridspec.html
-
-#gs1 = gridspec.GridSpec(4, 4)
-#gs1.update(wspace=0.025, hspace=0.05)  # set axis spacing
-
-#ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3) # top-left + center + right
-#ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=2) # mid-left + mid-center
-#ax3 = plt.subplot2grid((3, 3), (1, 2), rowspan=2) # mid-right + bot-right
-#ax4 = plt.subplot2grid((3, 3), (2, 0))            # bot-left
-#ax5 = plt.subplot2grid((3, 3), (2, 1))            # bot-center
-
-
-##--------------------------------------------------------------------------##
-#plt.style.use('bmh')   # Bayesian Methods for Hackers style
-fig_dims = (12, 10)
-fig = plt.figure(1, figsize=fig_dims)
-plt.gcf().clf()
-#fig, axs = plt.subplots(2, 2, sharex=True, figsize=fig_dims, num=1)
-# sharex='col' | sharex='row'
-#fig.frameon = False # disable figure frame drawing
-#fig.subplots_adjust(left=0.07, right=0.95)
-#ax1 = plt.subplot(gs[0, 0])
-#ax1 = fig.add_subplot(111)
-#ax1 = fig.add_axes([0, 0, 1, 1])
-#ax1.patch.set_facecolor((0.8, 0.8, 0.8))
-#ax1.grid(True)
-#ax1.axis('off')
-
-## Disable axis offsets:
-#ax1.xaxis.get_major_formatter().set_useOffset(False)
-#ax1.yaxis.get_major_formatter().set_useOffset(False)
-
-#ax1.plot(kde_pnts, kde_vals)
-
-#blurb = "some text"
-#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes)
-#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes,
-#      va='top', ha='left', bbox=dict(facecolor='white', pad=10.0))
-#      fontdict={'family':'monospace'}) # fixed-width
-
-#colors = cm.rainbow(np.linspace(0, 1, len(plot_list)))
-#for camid, c in zip(plot_list, colors):
-#    cam_data = subsets[camid]
-#    xvalue = cam_data['CCDATEMP']
-#    yvalue = cam_data['PIX_MED']
-#    yvalue = cam_data['IMEAN']
-#    ax1.scatter(xvalue, yvalue, color=c, lw=0, label=camid)
-
-#mtickpos = [2,5,7]
-#ndecades = 1.0   # for symlog, set width of linear portion in units of dex
-#nonposx='mask' | nonposx='clip' | nonposy='mask' | nonposy='clip'
-#ax1.set_xscale('log', basex=10, nonposx='mask', subsx=mtickpos)
-#ax1.set_xscale('log', nonposx='clip', subsx=[3])
-#ax1.set_yscale('symlog', basey=10, linthreshy=0.1, linscaley=ndecades)
-#ax1.xaxis.set_major_formatter(formatter) # re-format x ticks
-#ax1.set_ylim(ax1.get_ylim()[::-1])
-#ax1.set_xlabel('whatever', labelpad=30)  # push X label down 
-
-#ax1.set_xticks([1.0, 3.0, 10.0, 30.0, 100.0])
-#ax1.set_xticks([1, 2, 3], ['Jan', 'Feb', 'Mar'])
-#for label in ax1.get_xticklabels():
-#    label.set_rotation(30)
-#    label.set_fontsize(14) 
-
-#ax1.xaxis.label.set_fontsize(18)
-#ax1.yaxis.label.set_fontsize(18)
-
-#ax1.set_xlim(nice_limits(xvec, pctiles=[1,99], pad=1.2))
-#ax1.set_ylim(nice_limits(yvec, pctiles=[1,99], pad=1.2))
-
-#spts = ax1.scatter(x, y, lw=0, s=5)
-#cbar = fig.colorbar(spts, orientation='vertical')
-#cbar.formatter.set_useOffset(False)
-#cbar.update_ticks()
-
-#fig.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
-#plt.draw()
-#fig.savefig(plot_name, bbox_inches='tight')
-
-# cyclical colormap ... cmocean.cm.phase
-# cmocean: https://matplotlib.org/cmocean/
 
 
 
@@ -425,6 +174,6 @@ plt.gcf().clf()
 #---------------------------------------------------------------------
 #
 #  2019-10-15:
-#     -- Increased __version__ to 0.0.1.
+#     -- Increased __version__ to 0.1.0.
 #     -- First created spitz_extract.py.
 #
