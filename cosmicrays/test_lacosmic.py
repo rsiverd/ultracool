38b990f12d59830f371aef2760afba786f255b78
Rob Siverd <robert.siverd@gmail.com>
2019-10-24 14:43:52 -1000

    initial commit of cosmic ray removal testing notes/code

---
+++ cosmicrays/test_lacosmic.py
@@ -0,0 +1,25 @@
+
+%run ./extract_and_match_gaia.py -g ultracool/csv/gaia_wise1828.csv \ 
+   -i ucd_data/SPITZER_I2_61246976_0004_0000_1_cbcd.fits            \ 
+   -u ucd_data/SPITZER_I2_61246976_0004_0000_1_cbunc.fits 
+
+from lacosmic import lacosmic
+
+_mask_NaNs = True
+#_mask_NaNs = False
+
+lakw = {'contrast': 6.0,
+        'cr_threshold':4.0,
+        'neighbor_threshold':4.0,
+        }
+if _have_err_image:
+    lakw['error'] = udata
+if _mask_NaNs:
+    lakw['mask'] = np.isnan(idata)
+
+sys.stderr.write("Running LACOSMIC ... ")
+tik = time.time()
+clean_data, cr_mask = lacosmic(idata, **lakw)
+tok = time.time()
+sys.stderr.write("done. (%.3f s)\n" % (tok-tik))
+
