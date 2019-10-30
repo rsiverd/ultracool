69887e7c4a918026fdafe81125ee16ee3b4a064c
Rob Siverd <robert.siverd@gmail.com>
2019-10-29 23:36:25 -1000

    initial commit of all-image catalog extraction script

---
+++ 05_extract_all_spitzer.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Extract and save extended object catalogs from the specified data and
+# uncertainty images.
+#
+# Rob Siverd
+# Created:       2019-10-29
+# Last modified: 2019-10-29
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Logging setup:
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+## Current version:
+__version__ = "0.1.0"
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+import argparse
+#import shutil
+#import resource
+#import signal
+#import glob
+#import gc
+import os
+import sys
+import time
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+## Because obviously:
+#import warnings
+#if not sys.warnoptions:
+#    warnings.simplefilter("ignore", category=DeprecationWarning)
+#    warnings.simplefilter("ignore", category=UserWarning)
+#    warnings.simplefilter("ignore")
+#with warnings.catch_warnings():
+#    some_risky_activity()
+#with warnings.catch_warnings():
+#    warnings.filterwarnings("ignore", category=DeprecationWarning)
+#    import problem_child1, problem_child2
+
+##--------------------------------------------------------------------------##
+## Disable buffering on stdout/stderr:
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
+sys.stderr = Unbuffered(sys.stderr)
+
+##--------------------------------------------------------------------------##
+
+#unlimited = (resource.RLIM_INFINITY, resource.RLIM_INFINITY)
+#if (resource.getrlimit(resource.RLIMIT_DATA) == unlimited):
+#    resource.setrlimit(resource.RLIMIT_DATA,  (3e9, 6e9))
+#if (resource.getrlimit(resource.RLIMIT_AS) == unlimited):
+#    resource.setrlimit(resource.RLIMIT_AS, (3e9, 6e9))
+
+## Memory management:
+#def get_memory():
+#    with open('/proc/meminfo', 'r') as mem:
+#        free_memory = 0
+#        for i in mem:
+#            sline = i.split()
+#            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+#                free_memory += int(sline[1])
+#    return free_memory
+#
+#def memory_limit():
+#    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+#    resource.setrlimit(resource.RLIMIT_AS, (get_memory() * 1024 / 2, hard))
+
+### Measure memory used so far:
+#def check_mem_usage_MB():
+#    max_kb_used = float(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+#    return max_kb_used / 1000.0
+
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+## Catch interruption cleanly:
+#def signal_handler(signum, frame):
+#    sys.stderr.write("\nInterrupted!\n\n")
+#    sys.exit(1)
+#
+#signal.signal(signal.SIGINT, signal_handler)
+
+
+##--------------------------------------------------------------------------##
+##------------------         Parse Command Line             ----------------##
+##--------------------------------------------------------------------------##
+
+## Parse arguments and run script:
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+## Enable raw text AND display of defaults:
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                        argparse.RawDescriptionHelpFormatter):
+    pass
+
+## Parse the command line:
+if __name__ == '__main__':
+
+    # ------------------------------------------------------------------
+    prog_name = os.path.basename(__file__)
+    descr_txt = """
+    Extract catalogs from the listed Spitzer data/uncertainty images.
+    
+    Version: %s
+    """ % __version__
+    parser = MyParser(prog=prog_name, description=descr_txt)
+                          #formatter_class=argparse.RawTextHelpFormatter)
+    # ------------------------------------------------------------------
+    #parser.set_defaults(thing1='value1', thing2='value2')
+    # ------------------------------------------------------------------
+    #parser.add_argument('firstpos', help='first positional argument')
+    parser.add_argument('-w', '--whatever', required=False, default=5.0,
+            help='some option with default [def: %(default)s]', type=float)
+    parser.add_argument('-s', '--site',
+            help='Site to retrieve data for', required=True)
+    parser.add_argument('-n', '--number_of_days', default=1,
+            help='Number of days of data to retrieve.')
+    parser.add_argument('-o', '--output_file', 
+            default='observations.csv', help='Output filename.')
+    parser.add_argument('-s', '--site', nargs=1, required=False,
+            help='Site to make URL for', choices=all_sites, default=all_sites)
+    # ------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    #iogroup = parser.add_argument_group('File I/O')
+    iogroup.add_argument('-I', '--input_folder', default=None, required=True,
+            help='where to find input images', type=str)
+    iogroup.add_argument('-O', '--output_folder', default=None, required=True,
+            help='where to save extended catalog outputs', type=str)
+    #iogroup.add_argument('-R', '--ref_image', default=None, required=True,
+    #        help='KELT image with WCS')
+    # ------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    # Miscellany:
+    miscgroup = parser.add_argument_group('Miscellany')
+    miscgroup.add_argument('--debug', dest='debug', default=False,
+            help='Enable extra debugging messages', action='store_true')
+    miscgroup.add_argument('-q', '--quiet', action='count', default=0,
+            help='less progress/status reporting')
+    miscgroup.add_argument('-v', '--verbose', action='count', default=0,
+            help='more progress/status reporting')
+    # ------------------------------------------------------------------
+
+    context = parser.parse_args()
+    context.vlevel = 99 if context.debug else (context.verbose-context.quiet)
+    context.prog_name = prog_name
+
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+## New-style string formatting (more at https://pyformat.info/):
+##--------------------------------------------------------------------------##
+## Quick ASCII I/O:
+#data_file = 'data.txt'
+#gftkw = {'encoding':None} if (_have_np_vers >= 1.14) else {}
+#gftkw.update({'names':True, 'autostrip':True})
+#gftkw.update({'delimiter':'|', 'comments':'%0%0%0%0'})
+#gftkw.update({'loose':True, 'invalid_raise':False})
+#all_data = np.genfromtxt(data_file, dtype=None, **gftkw)
+#all_data = aia.read(data_file)
+
+#all_data = pd.read_csv(data_file)
+#all_data = pd.read_table(data_file, delim_whitespace=True)
+#all_data = pd.read_table(data_file, sep='|')
+#fields = all_data.dtype.names
+#if not fields:
+#    x = all_data[:, 0]
+#    y = all_data[:, 1]
+#else:
+#    x = all_data[fields[0]]
+#    y = all_data[fields[1]]
+
+#vot_file = 'neato.xml'
+#vot_data = av.parse_single_table(vot_file)
+#vot_data = av.parse_single_table(vot_file).to_table()
+
+
+
+
+######################################################################
+# CHANGELOG (05_extract_all_spitzer.py):
+#---------------------------------------------------------------------
+#
+#  2019-10-29:
+#     -- Increased __version__ to 0.1.0.
+#     -- First created 05_extract_all_spitzer.py.
+#
