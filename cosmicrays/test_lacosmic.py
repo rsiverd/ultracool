e510fa9944cf66b810cb6efccea2a675fd629d22
Rob Siverd <robert.siverd@gmail.com>
2019-10-30 00:35:53 -1000

    adjusted contrast (seems to work much better now, not catching stars)

--- cosmicrays/test_lacosmic.py
+++ cosmicrays/test_lacosmic.py
@@ -8,8 +8,8 @@ from lacosmic import lacosmic
 _mask_NaNs = True
 #_mask_NaNs = False
 
-lakw = {'contrast': 6.0,
-        'cr_threshold':4.0,
+lakw = {'contrast': 12.0,
+        'cr_threshold':6.0,
         'neighbor_threshold':4.0,
         }
 if _have_err_image:
@@ -22,4 +22,11 @@ tik = time.time()
 clean_data, cr_mask = lacosmic(idata, **lakw)
 tok = time.time()
 sys.stderr.write("done. (%.3f s)\n" % (tok-tik))
+
+
+tag = '%d.%d' % (lakw['contrast'], lakw['cr_threshold'])
+cln_name = 'clean_image.%s.fits' % tag
+msk_name = 'cosmic_mask.%s.fits' % tag
+qsave(cln_name, clean_data)
+qsave(msk_name, cr_mask.astype('uint8'))
 
