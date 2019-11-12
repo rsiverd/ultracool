708c659410ab2a2aab12758bff5535ab62f2924e
Rob Siverd <robert.siverd@gmail.com>
2019-11-11 16:23:32 -1000

    initial commit of environment setup script. Currently this just adds the modules subfolder to PYTHONPATH if not already present

---
+++ ENVIRONMENT.sh
@@ -0,0 +1,24 @@
+# Update PYTHONPATH to include 'modules' project subfolder.
+
+here_base=$(basename $(pwd))
+#echo "here_base: $here_base"
+if [ "$here_base" != "ultracool" ]; then
+   echo "Please source this inside the 'ultracool' project folder!" >&2
+   return 1
+fi
+if [ ! -d modules ]; then
+   echo "Cannot find 'modules' folder!  Something is very wrong ..." >&2
+   return 1
+fi
+module_dir="`pwd`/modules"
+echo "module_dir: $module_dir"
+#echo $PYTHONPATH
+is_dupe=`echo $PYTHONPATH | tr ':' '\n' | grep "^${module_dir}$" | wc -l`
+if [ $is_dupe -gt 0 ]; then
+   echo "PYTHONPATH already configured!"
+   return 0
+fi
+new_pypath=$(echo "${module_dir}:${PYTHONPATH}" | sed 's/:$//')
+#echo "new_pypath: '$new_pypath'"
+export PYTHONPATH="$new_pypath"
+echo "PYTHONPATH updated."
