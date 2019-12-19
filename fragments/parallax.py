1fdd83dc37893b843f42d90e92c49b3a5dc1b1f3
Rob Siverd <robert.siverd@gmail.com>
2019-12-19 02:54:07 -1000

    factored out computation of RA/DE parallax factors (this will in practice be used separately in order to compute the parallax)

--- fragments/parallax.py
+++ fragments/parallax.py
@@ -3,6 +3,27 @@
 
 import numpy as np
 
+## Compute parallax factors:
+def parallax_factors(meanRA, meanDE, X, Y, Z):
+    """Compute RA offset due to parallax given solar system XYZ position.
+
+    Inputs:
+    meanRA      --  mean/nominal RA coordinate (degrees)
+    meanDE      --  mean/nominal DE coordinate (degrees)
+    X, Y, Z     --  solar system barycentric coordinates (in AU)
+
+    Returns:
+    pfRA, pfDE -- tuple of RA, DE parallax factors
+    """
+    rra = np.radians(meanRA)
+    rde = np.radians(meanDE)
+    ra_factor = (X * np.sin(rra) - Y * np.cos(rra)) / np.cos(rde)
+    de_factor =   X * np.cos(rra) * np.sin(rde) \
+                + Y * np.sin(rra) * np.sin(rde) \
+                - Z * np.cos(rde)
+    return ra_factor, de_factor
+
+
 ## Compute RA offset due to parallax:
 def parallax_offsets(meanRA, meanDE, X, Y, Z, parallax):
     """Compute RA offset due to parallax given solar system XYZ position.
@@ -17,12 +38,8 @@ def parallax_offsets(meanRA, meanDE, X, Y, Z, parallax):
     RA_shift    --  parallax adjustment in RA (same units as parallax)
     DE_shift    --  parallax adjustment in DE (same units as parallax)
     """
-    rra = np.radians(meanRA)
-    rde = np.radians(meanDE)
-    ra_factor = (X * np.sin(rra) - Y * np.cos(rra)) / np.cos(rde)
-    de_factor =   X * np.cos(rra) * np.sin(rde) \
-                + Y * np.sin(rra) * np.sin(rde) \
-                - Z * np.cos(rde)
+    ra_factor, de_factor = parallax_factors(meanRA, meanDE, X, Y, Z)
     return (parallax * ra_factor, parallax * de_factor)
+
 
 
