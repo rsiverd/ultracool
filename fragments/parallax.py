1b6d6d6f998eb1e0ba6cd020fabe22b626c5dc7d
Rob Siverd <robert.siverd@gmail.com>
2020-02-06 17:25:45 -1000

    added parallax factor calculation routine for radian units. added apparent ra,dec position calculator (also radians). established nominal units for solvers that require unitless numpy parameter arrays.

--- fragments/parallax.py
+++ fragments/parallax.py
@@ -23,6 +23,14 @@ def parallax_factors(meanRA, meanDE, X, Y, Z):
                 - Z * np.cos(rde)
     return ra_factor, de_factor
 
+def calc_parallax_factors(RA_rad, DE_rad, X_au, Y_au, Z_au):
+    sinRA, cosRA = np.sin(RA_rad), np.cos(RA_rad)
+    sinDE, cosDE = np.sin(DE_rad), np.cos(DE_rad)
+    ra_factor = (X_au * sinRA - Y_au * cosRA) / cosDE
+    de_factor =  X_au * cosRA * sinDE \
+              +  Y_au * sinRA * sinDE \
+              -  Z_au * cosDE
+    return ra_factor, de_factor
 
 ## Compute RA offset due to parallax:
 def parallax_offsets(meanRA, meanDE, X, Y, Z, parallax):
@@ -42,4 +50,39 @@ def parallax_offsets(meanRA, meanDE, X, Y, Z, parallax):
     return (parallax * ra_factor, parallax * de_factor)
 
 
+## Compute apparent positions given true positions and time of observation:
+_ARCSEC_PER_RADIAN = 180. * 3600.0 / np.pi
+def apparent_radec(t_ref, astrom_pars, eph_obs):
+    """
+    t_ref       --  chosen reference epoch
+    astrom_pars --  five astrometric parameters specified at the
+                    reference epoch: meanRA (rad), meanDE (rad),
+                    pmRA*cos(DE), pmDE, and parallax
+    eph_obs     --  dict with x,y,z,t elements describing the times
+                    and places of observations (numpy arrays)
+    FOR NOW, assume
+                [t_ref] = JD (TDB)
+                [t]     = JD (TDB)
+                [pars]  = rad, rad, arcsec/yr, arcsec/yr, arcsec
+                                   *no cos(d)*
+    """
+
+    rra, rde, pmra, pmde, prlx = astrom_pars
+    
+    t_diff_yr = (eph_obs['t'] - t_ref) / 365.25     # units of years
+
+    pfra, pfde = calc_parallax_factors(rra, rde,
+            eph_obs['x'], eph_obs['y'], eph_obs['z'])
+
+    delta_ra = (t_diff_yr * pmra / _ARCSEC_PER_RADIAN) + (prlx * pfra)
+    delta_de = (t_diff_yr * pmde / _ARCSEC_PER_RADIAN) + (prlx * pfde)
+
+    return (rra + delta_ra, rde + delta_de)
+
+import astropy.units as uu
+_par_keys = ['ra', 'de', 'pmra', 'pmde', 'plx']
+_par_vals = [123.45, 54.32, 1.23, 4.56, 0.4]
+_par_unit = [uu.deg, uu.deg, uu.arcsec/uu.year,
+        uu.arcsec/uu.year, uu.arcsec]
+data = np.array(_par_vals) * np.array(_par_unit)
 
