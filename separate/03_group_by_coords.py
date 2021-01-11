e81de68441d158d7193b91d13846697e70e9c3ab
Rob Siverd <robert.siverd@gmail.com>
2021-01-11 10:32:53 -1000

    initial commit of grouping script

---
+++ separate/03_group_by_coords.py
@@ -0,0 +1,359 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Select fcat files corresponding to specific parts of the sky.
+#
+# Rob Siverd
+# Created:       2021-01-11
+# Last modified: 2021-01-11
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Current version:
+__version__ = "0.1.0"
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+#import argparse
+#import shutil
+#import signal
+#import glob
+import gc
+import os
+import sys
+import time
+#import vaex
+#import calendar
+#import ephem
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+#import matplotlib.ticker as mt
+#import matplotlib._pylab_helpers as hlp
+#from matplotlib.colors import LogNorm
+#import matplotlib.colors as mplcolors
+#import matplotlib.collections as mcoll
+#import matplotlib.gridspec as gridspec
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import PIL.Image as pli
+#import seaborn as sns
+#import cmocean
+#import theil_sen as ts
+#import window_filter as wf
+#import itertools as itt
+_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+## Because obviously:
+#import warnings
+#if not sys.warnoptions:
+#    warnings.simplefilter("ignore", category=DeprecationWarning)
+#    warnings.simplefilter("ignore", category=UserWarning)
+#    warnings.simplefilter("ignore")
+#with warnings.catch_warnings():
+#    some_risky_activity()
+#with warnings.catch_warnings():
+#    warnings.filterwarnings("ignore", category=DeprecationWarning)
+#    import problem_child1, problem_child2
+
+##--------------------------------------------------------------------------##
+## Projections with cartopy:
+#try:
+#    import cartopy.crs as ccrs
+#    from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+#    from cartopy.feature.nightshade import Nightshade
+#    #from cartopy import config as cartoconfig
+#except ImportError:
+#    sys.stderr.write("Error: cartopy module not found!\n")
+#    sys.exit(1)
+
+##--------------------------------------------------------------------------##
+## Disable buffering on stdout/stderr:
+#class Unbuffered(object):
+#   def __init__(self, stream):
+#       self.stream = stream
+#   def write(self, data):
+#       self.stream.write(data)
+#       self.stream.flush()
+#   def __getattr__(self, attr):
+#       return getattr(self.stream, attr)
+#
+#sys.stdout = Unbuffered(sys.stdout)
+#sys.stderr = Unbuffered(sys.stderr)
+
+##--------------------------------------------------------------------------##
+
+## Home-brew robust statistics:
+#try:
+#    import robust_stats
+#    reload(robust_stats)
+#    rs = robust_stats
+#except ImportError:
+#    logger.error("module robust_stats not found!  Install and retry.")
+#    sys.stderr.write("\nError!  robust_stats module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Home-brew KDE:
+#try:
+#    import my_kde
+#    reload(my_kde)
+#    mk = my_kde
+#except ImportError:
+#    logger.error("module my_kde not found!  Install and retry.")
+#    sys.stderr.write("\nError!  my_kde module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Fast FITS I/O:
+#try:
+#    import fitsio
+#except ImportError:
+#    logger.error("fitsio module not found!  Install and retry.")
+#    sys.stderr.write("\nError: fitsio module not found!\n")
+#    sys.exit(1)
+
+## Various from astropy:
+#try:
+#    import astropy.io.ascii as aia
+#    import astropy.io.fits as pf
+#    import astropy.io.votable as av
+#    import astropy.table as apt
+#    import astropy.time as astt
+#    import astropy.wcs as awcs
+#    from astropy import constants as aconst
+#    from astropy import coordinates as coord
+#    from astropy import units as uu
+#except ImportError:
+#    logger.error("astropy module not found!  Install and retry.")
+#    sys.stderr.write("\nError: astropy module not found!\n")
+#    sys.exit(1)
+
+## Star extraction:
+#try:
+#    import easy_sep
+#    reload(easy_sep)
+#except ImportError:
+#    logger.error("easy_sep module not found!  Install and retry.")
+#    sys.stderr.write("Error: easy_sep module not found!\n\n")
+#    sys.exit(1)
+#pse = easy_sep.EasySEP()
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (astropy / pyfits):
+#def qsave(iname, idata, header=None, padkeys=1000, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    if header:
+#        while (len(header) < padkeys):
+#            header.append() # pad header
+#    if os.path.isfile(iname):
+#        os.remove(iname)
+#    pf.writeto(iname, idata, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (fitsio):
+#def qsave(iname, idata, header=None, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    #if os.path.isfile(iname):
+#    #    os.remove(iname)
+#    fitsio.write(iname, idata, clobber=True, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+def ldmap(things):
+    return dict(zip(things, range(len(things))))
+
+def argnear(vec, val):
+    return (np.abs(vec - val)).argmin()
+
+
+
+
+##--------------------------------------------------------------------------##
+##------------------         Parse Command Line             ----------------##
+##--------------------------------------------------------------------------##
+
+## Parse arguments and run script:
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+## Enable raw text AND display of defaults:
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                        argparse.RawDescriptionHelpFormatter):
+    pass
+
+## Parse the command line:
+if __name__ == '__main__':
+
+    # ------------------------------------------------------------------
+    prog_name = os.path.basename(__file__)
+    descr_txt = """
+    Separate fcat files according to object.
+    
+    Version: %s
+    """ % __version__
+    parser = MyParser(prog=prog_name, description=descr_txt,
+                          formatter_class=argparse.RawTextHelpFormatter)
+    # ------------------------------------------------------------------
+    #parser.set_defaults(thing1='value1', thing2='value2')
+    parser.set_defaults(fcat_list='fcat_list.txt')
+    parser.set_defaults(targ_list='targets_deg.txt')
+    # ------------------------------------------------------------------
+    #parser.add_argument('firstpos', help='first positional argument')
+    #parser.add_argument('-w', '--whatever', required=False, default=5.0,
+    #        help='some option with default [def: %(default)s]', type=float)
+    #parser.add_argument('-s', '--site',
+    #        help='Site to retrieve data for', required=True)
+    #parser.add_argument('-n', '--number_of_days', default=1,
+    #        help='Number of days of data to retrieve.')
+    #parser.add_argument('-o', '--output_file', 
+    #        default='observations.csv', help='Output filename.')
+    #parser.add_argument('--start', type=str, default=None, 
+    #        help="Start time for date range query.")
+    #parser.add_argument('--end', type=str, default=None,
+    #        help="End time for date range query.")
+    #parser.add_argument('-d', '--dayshift', required=False, default=0,
+    #        help='Switch between days (1=tom, 0=today, -1=yest', type=int)
+    #parser.add_argument('-e', '--encl', nargs=1, required=False,
+    #        help='Encl to make URL for', choices=all_encls, default=all_encls)
+    #parser.add_argument('-s', '--site', nargs=1, required=False,
+    #        help='Site to make URL for', choices=all_sites, default=all_sites)
+    #parser.add_argument('remainder', help='other stuff', nargs='*')
+    # ------------------------------------------------------------------
+    # ------------------------------------------------------------------
+    #iogroup = parser.add_argument_group('File I/O')
+    #iogroup.add_argument('-o', '--output_file', default=None, required=True,
+    #        help='Output filename', type=str)
+    #iogroup.add_argument('-R', '--ref_image', default=None, required=True,
+    #        help='KELT image with WCS')
+    # ------------------------------------------------------------------
+    # Miscellany:
+    miscgroup = parser.add_argument_group('Miscellany')
+    miscgroup.add_argument('--debug', dest='debug', default=False,
+            help='Enable extra debugging messages', action='store_true')
+    miscgroup.add_argument('-q', '--quiet', action='count', default=0,
+            help='less progress/status reporting')
+    miscgroup.add_argument('-v', '--verbose', action='count', default=0,
+            help='more progress/status reporting')
+    # ------------------------------------------------------------------
+
+    context = parser.parse_args()
+    context.vlevel = 99 if context.debug else (context.verbose-context.quiet)
+    context.prog_name = prog_name
+
+##--------------------------------------------------------------------------##
+## Load list of objects and coordinates:
+
+
+
+
+##--------------------------------------------------------------------------##
+## Quick ASCII I/O:
+#data_file = 'data.txt'
+#gftkw = {'encoding':None} if (_have_np_vers >= 1.14) else {}
+#gftkw.update({'names':True, 'autostrip':True})
+#gftkw.update({'delimiter':'|', 'comments':'%0%0%0%0'})
+#gftkw.update({'loose':True, 'invalid_raise':False})
+#all_data = np.genfromtxt(data_file, dtype=None, **gftkw)
+#all_data = aia.read(data_file)
+
+#all_data = pd.read_csv(data_file)
+#all_data = pd.read_table(data_file, delim_whitespace=True)
+#all_data = pd.read_table(data_file, skipinitialspace=True)
+#all_data = pd.read_table(data_file, sep='|')
+#fields = all_data.dtype.names
+#if not fields:
+#    x = all_data[:, 0]
+#    y = all_data[:, 1]
+#else:
+#    x = all_data[fields[0]]
+#    y = all_data[fields[1]]
+
+#vot_file = 'neato.xml'
+#vot_data = av.parse_single_table(vot_file)
+#vot_data = av.parse_single_table(vot_file).to_table()
+
+##--------------------------------------------------------------------------##
+## Timestamp modification:
+#def time_warp(jdutc, jd_offset, scale):
+#    return (jdutc - jd_offset) * scale
+
+## Self-consistent time-modification for plotting:
+#tfudge = partial(time_warp, jd_offset=tstart.jd, scale=24.0)    # relative hrs
+#tfudge = partial(time_warp, jd_offset=tstart.jd, scale=1440.0)  # relative min
+
+##--------------------------------------------------------------------------##
+## Quick FITS I/O:
+#data_file = 'image.fits'
+#img_vals = pf.getdata(data_file)
+#hdr_keys = pf.getheader(data_file)
+#img_vals, hdr_keys = pf.getdata(data_file, header=True)
+#img_vals, hdr_keys = pf.getdata(data_file, header=True, uint=True) # USHORT
+#img_vals, hdr_keys = fitsio.read(data_file, header=True)
+
+#date_obs = hdr_keys['DATE-OBS']
+#site_lat = hdr_keys['LATITUDE']
+#site_lon = hdr_keys['LONGITUD']
+
+## Initialize time:
+#img_time = astt.Time(hdr_keys['DATE-OBS'], scale='utc', format='isot')
+#img_time += astt.TimeDelta(0.5 * hdr_keys['EXPTIME'], format='sec')
+#jd_image = img_time.jd
+
+## Initialize location:
+#observer = ephem.Observer()
+#observer.lat = np.radians(site_lat)
+#observer.lon = np.radians(site_lon)
+#observer.date = img_time.datetime
+
+#pf.writeto('new.fits', img_vals)
+#qsave('new.fits', img_vals)
+#qsave('new.fits', img_vals, header=hdr_keys)
+
+## Star extraction:
+#pse.set_image(img_vals, gain=3.6)
+#objlist = pse.analyze(sigthresh=5.0)
+
+
+
+######################################################################
+# CHANGELOG (03_group_by_coords.py):
+#---------------------------------------------------------------------
+#
+#  2021-01-11:
+#     -- Increased __version__ to 0.1.0.
+#     -- First created 03_group_by_coords.py.
+#
