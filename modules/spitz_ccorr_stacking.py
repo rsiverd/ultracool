2ac1bfe55515f1fd640fec54b874d395c2b3103a
Rob Siverd <robert.siverd@gmail.com>
2021-02-03 16:01:00 -1000

    xcorr registration and stacking functionality fully implemented.

--- modules/spitz_ccorr_stacking.py
+++ modules/spitz_ccorr_stacking.py
@@ -8,7 +8,7 @@
 #
 # Rob Siverd
 # Created:       2021-02-02
-# Last modified: 2021-02-02
+# Last modified: 2021-02-03
 #--------------------------------------------------------------------------
 #**************************************************************************
 #--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 ## Current version:
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 ## Python version-agnostic module reloading:
 try:
@@ -56,6 +56,16 @@ import numpy as np
 #import window_filter as wf
 #import itertools as itt
 _have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+## RJS personal angular routines:
+try:
+    import angle
+    reload(angle)
+except ImportError:
+    logger.error("module 'angle' not found!  Install and retry.")
+    sys.stderr.write("\nError!  robust_stats module not found!\n"
+           "Please install and try again ...\n\n")
+    sys.exit(1)
 
 ##--------------------------------------------------------------------------##
 
@@ -132,15 +142,20 @@ class SpitzerXCorr(object):
         return
 
     def _reset(self):
-        self._dimen    = (None, None)
+        self._ishape   = (None, None)
         self._padpix   = 0
+        self._medoid   = None
+        self._ref_idx  = 0
+        self._imstack  = None
         self._im_paths = []
-        self._im_data  = []
+        self._im_hdrs  = [] # image header content
+        self._im_data  = [] # image pixel data
         self._bp_masks = []
         self._row_sums = [] # formerly xsmashed
         self._col_sums = [] # formerly ysmashed
         self._x_shifts = []
         self._y_shifts = []
+        self._reg_data = [] # registered images
         return
 
     # --------------------------------------------------------- #
@@ -156,6 +171,22 @@ class SpitzerXCorr(object):
         return
 
     # --------------------------------------------------------- #
+    #                Data Product Retrieval:                    #
+    # --------------------------------------------------------- #
+
+    def get_xy_offsets(self):
+        """Returns the lists of X- and Y- pixel offsets."""
+        return self._x_shifts, self._y_shifts
+
+    def get_aligned(self):
+        """Returns a list of padded, cross-correlation-aligned frames."""
+        return self._reg_data
+
+    def get_stacked(self):
+        """Returns the stacked image."""
+        return self._imstack
+
+    # --------------------------------------------------------- #
     #                  High-Level Routines:                     #
     # --------------------------------------------------------- #
 
@@ -168,10 +199,16 @@ class SpitzerXCorr(object):
         # generate bright pixel masks:
         self._make_bp_masks(thresh=self._bp_thresh)
 
+        # choose central xcorr reference to maximize overlap:
+        self._choose_reference_image()
+
         # UNPADDED cross-correlation to find pixel shifts.
         # Sum across rows to produce average column along columns
         # for average row:
         self._run_xy_xcorr(self._bp_masks)
+
+        # Pad and shift input frames, stack result:
+        self._run_shift_and_stack()
 
         return result
 
@@ -179,31 +216,38 @@ class SpitzerXCorr(object):
     #    result = 
 
     # --------------------------------------------------------- #
+    #               Reference Image Selection:                  #
+    # --------------------------------------------------------- #
+
+    # Select a 'central' frame for registration and note its index for
+    # later use. Smart choice of this image maximizes overlap and decreases
+    # potential for false positive cross-correlation results.
+    def _choose_reference_image(self):
+        self._medoid  = self._find_medoid_image()
+        self._ref_idx = self._medoid['idx']
+        return
+
+    # This routine uses header WCS (CRVAL1/2) to identify a 'central'
+    # image to which images should be shifted. The 'medoid' image is that
+    # which minimizes the sum of absolute (angular) deviations of the set.
+    def _find_medoid_image(self):
+        imra = np.array([hh['CRVAL1'] for hh in self._im_hdrs])
+        imde = np.array([hh['CRVAL2'] for hh in self._im_hdrs])
+        middle = angle.sphere_medoid_deg(imra, imde)
+        return middle
+
+    # --------------------------------------------------------- #
     #               Cross-Correlation Helpers:                  #
     # --------------------------------------------------------- #
 
-
     def _load_frames(self, img_list):
+        self._reset()   # wipe existing info
         self._im_paths = [x for x in img_list]
         self._im_data  = [self._nanfix(fitsio.read(ff)) \
                 for ff in self._im_paths]
-        self._dimen = self._im_data[0].shape
-        self._padpix = max([int(0.5 * pix) for pix in self._dimen])
-        return
-
-    # Cross-correlation of reduced bright pixel masks:
-    def _run_xy_xcorr(self, frames):
-        # UNPADDED cross-correlation to find pixel shifts.
-        # Sum across rows to produce average column along columns
-        # for average row:
-        xsmashed = [np.sum(im, axis=1) for im in frames]    # sum each row
-        ysmashed = [np.sum(im, axis=0) for im in frames]    # sum each col
-        self._row_sums, self._col_sums = xsmashed, ysmashed
-
-        # Cross-correlate to find pixel shifts:
-        xnudges = [self.qcorr(ysmashed[0], rr) for rr in ysmashed]
-        ynudges = [self.qcorr(xsmashed[0], cc) for cc in xsmashed]
-        self._x_shifts, self._y_shifts = xnudges, ynudges
+        self._im_hdrs  = [fitsio.read_header(ff) for ff in self._im_paths]
+        self._ishape = self._im_data[0].shape
+        self._padpix = max([int(0.5 * pix) for pix in self._ishape])
         return
 
     @staticmethod
@@ -221,6 +265,22 @@ class SpitzerXCorr(object):
             pix_med, pix_iqrn = rs.calc_ls_med_IQR(frame)
             bright = (frame - pix_med >= thresh * pix_iqrn)
             self._bp_masks.append(bright)
+        return
+
+    # Cross-correlation of reduced bright pixel masks:
+    def _run_xy_xcorr(self, frames):
+        # UNPADDED cross-correlation to find pixel shifts.
+        # Sum across rows to produce average column along columns
+        # for average row:
+        xsmashed = [np.sum(im, axis=1) for im in frames]    # sum each row
+        ysmashed = [np.sum(im, axis=0) for im in frames]    # sum each col
+        self._row_sums, self._col_sums = xsmashed, ysmashed
+
+        # Cross-correlate to find pixel shifts:
+        which   = self._ref_idx
+        xnudges = [self.qcorr(ysmashed[which], rr) for rr in ysmashed]
+        ynudges = [self.qcorr(xsmashed[which], cc) for cc in xsmashed]
+        self._x_shifts, self._y_shifts = xnudges, ynudges
         return
 
     # 1-D cross-correlation driver routine:
@@ -254,11 +314,31 @@ class SpitzerXCorr(object):
         sys.stderr.write("--------------------------------\n")
         return
 
+    # --------------------------------------------------------- #
+    #                 Image Stacking Helpers:                   #
+    # --------------------------------------------------------- #
+
+    # Pad+shift and stacking driver routine:
+    def _run_shift_and_stack(self):
+        self._reg_data = self._pad_and_shift(self._im_data,
+                self._padpix, self._x_shifts, self._y_shifts)
+        self._imstack  = self.dumb_stack(self._reg_data)
+        return
+
+    # Pad and shift a set of frames:
+    def _pad_and_shift(self, frames, padding, xnudges, ynudges):
+        layers = []
+        for im,dx,dy in zip(frames, xnudges, ynudges):
+            pdata = np.pad(im, padding, constant_values=np.nan)
+            rdata = np.roll(np.roll(pdata, dx, axis=1), dy, axis=0)
+            layers.append(rdata)
+        return layers
+
     # Median-combination of listed overlapping frames:
     @staticmethod
-    def dumb_stack(im_list):
-        tstack = np.median(im_list, axis=0)
-        for im in im_list:
+    def dumb_stack(frames):
+        tstack = np.median(frames, axis=0)
+        for im in frames:
             which = np.isnan(im) | np.isinf(im)
             tstack[which] = np.nan
         return tstack
@@ -271,6 +351,10 @@ class SpitzerXCorr(object):
 ######################################################################
 # CHANGELOG (spitz_stacking.py):
 #---------------------------------------------------------------------
+#
+#  2021-02-03:
+#     -- Increased __version__ to 0.2.0.
+#     -- Basic module functionality successfully implemented.
 #
 #  2021-02-02:
 #     -- Increased __version__ to 0.1.0.
