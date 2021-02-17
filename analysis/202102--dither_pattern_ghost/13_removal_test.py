39fbb654d20661f7037f5e952ac6f4449c15d367
Rob Siverd <robert.siverd@gmail.com>
2021-02-16 17:15:43 -1000

    initial commit of removal test script and output plot

---
+++ analysis/202102--dither_pattern_ghost/13_removal_test.py
@@ -0,0 +1,407 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Attempt removal of fading residual ghost image from image set.
+#
+# Rob Siverd
+# Created:       2021-02-16
+# Last modified: 2021-02-16
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Logging setup:
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+## Current version:
+__version__ = "0.0.1"
+
+## Optional matplotlib control:
+#from matplotlib import use, rc, rcParams
+#from matplotlib import use
+#from matplotlib import rc
+#from matplotlib import rcParams
+#use('GTKAgg')  # use GTK with Anti-Grain Geometry engine
+#use('agg')     # use Anti-Grain Geometry engine (file only)
+#use('ps')      # use PostScript engine for graphics (file only)
+#use('cairo')   # use Cairo (pretty, file only)
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('font',**{'family':'serif','serif':['Palatino']})
+#rc('font',**{'sans-serif':'Arial','family':'sans-serif'})
+#rc('text', usetex=True) # enables text rendering with LaTeX (slow!)
+#rcParams['axes.formatter.useoffset'] = False   # v. 1.4 and later
+#rcParams['agg.path.chunksize'] = 10000
+#rcParams['font.size'] = 10
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+#import argparse
+import shutil
+#import resource
+#import signal
+import glob
+import os
+import sys
+import time
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+#import matplotlib.ticker as mt
+#import matplotlib._pylab_helpers as hlp
+#from matplotlib.colors import LogNorm
+#import matplotlib.colors as mplcolors
+#import matplotlib.collections as mcoll
+#import matplotlib.gridspec as gridspec
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import PIL.Image as pli
+#import seaborn as sns
+#import cmocean
+#import theil_sen as ts
+#import window_filter as wf
+#import itertools as itt
+_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+##--------------------------------------------------------------------------##
+
+## Home-brew robust statistics:
+try:
+    import robust_stats
+    reload(robust_stats)
+    rs = robust_stats
+except ImportError:
+    logger.error("module robust_stats not found!  Install and retry.")
+    sys.stderr.write("\nError!  robust_stats module not found!\n"
+           "Please install and try again ...\n\n")
+    sys.exit(1)
+
+## Fast FITS I/O:
+#try:
+#    import fitsio
+#except ImportError:
+#    logger.error("fitsio module not found!  Install and retry.")
+#    sys.stderr.write("\nError: fitsio module not found!\n")
+#    sys.exit(1)
+
+## Various from astropy:
+try:
+#    import astropy.io.ascii as aia
+    import astropy.io.fits as pf
+#    import astropy.io.votable as av
+#    import astropy.table as apt
+    import astropy.time as astt
+#    import astropy.wcs as awcs
+#    from astropy import constants as aconst
+#    from astropy import coordinates as coord
+#    from astropy import units as uu
+except ImportError:
+    logger.error("astropy module not found!  Install and retry.")
+    sys.stderr.write("\nError: astropy module not found!\n")
+    sys.exit(1)
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (astropy / pyfits):
+def qsave(iname, idata, header=None, padkeys=1000, **kwargs):
+    this_func = sys._getframe().f_code.co_name
+    parent_func = sys._getframe(1).f_code.co_name
+    sys.stderr.write("Writing to '%s' ... " % iname)
+    if header:
+        while (len(header) < padkeys):
+            header.append() # pad header
+    if os.path.isfile(iname):
+        os.remove(iname)
+    pf.writeto(iname, idata, header=header, **kwargs)
+    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (fitsio):
+#def qsave(iname, idata, header=None, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    #if os.path.isfile(iname):
+#    #    os.remove(iname)
+#    fitsio.write(iname, idata, clobber=True, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+
+##--------------------------------------------------------------------------##
+##------------------         Parse Command Line             ----------------##
+##--------------------------------------------------------------------------##
+
+## Parse arguments and run script:
+#class MyParser(argparse.ArgumentParser):
+#    def error(self, message):
+#        sys.stderr.write('error: %s\n' % message)
+#        self.print_help()
+#        sys.exit(2)
+#
+### Enable raw text AND display of defaults:
+#class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+#                        argparse.RawDescriptionHelpFormatter):
+#    pass
+#
+### Parse the command line:
+#if __name__ == '__main__':
+#
+#    # ------------------------------------------------------------------
+#    prog_name = os.path.basename(__file__)
+#    descr_txt = """
+#    PUT DESCRIPTION HERE.
+#    
+#    Version: %s
+#    """ % __version__
+#    parser = argparse.ArgumentParser(
+#            prog='PROGRAM_NAME_HERE',
+#            prog=os.path.basename(__file__),
+#            #formatter_class=argparse.RawTextHelpFormatter)
+#            description='PUT DESCRIPTION HERE.')
+#            #description=descr_txt)
+#    parser = MyParser(prog=prog_name, description=descr_txt)
+#                          #formatter_class=argparse.RawTextHelpFormatter)
+#    # ------------------------------------------------------------------
+#    parser.set_defaults(thing1='value1', thing2='value2')
+#    # ------------------------------------------------------------------
+#    parser.add_argument('firstpos', help='first positional argument')
+#    parser.add_argument('-w', '--whatever', required=False, default=5.0,
+#            help='some option with default [def: %(default)s]', type=float)
+#    parser.add_argument('-s', '--site',
+#            help='Site to retrieve data for', required=True)
+#    parser.add_argument('-n', '--number_of_days', default=1,
+#            help='Number of days of data to retrieve.')
+#    parser.add_argument('-o', '--output_file', 
+#            default='observations.csv', help='Output filename.')
+#    parser.add_argument('--start', type=str, default=None, 
+#            help="Start time for date range query.")
+#    parser.add_argument('--end', type=str, default=None,
+#            help="End time for date range query.")
+#    parser.add_argument('-d', '--dayshift', required=False, default=0,
+#            help='Switch between days (1=tom, 0=today, -1=yest', type=int)
+#    parser.add_argument('-e', '--encl', nargs=1, required=False,
+#            help='Encl to make URL for', choices=all_encls, default=all_encls)
+#    parser.add_argument('-s', '--site', nargs=1, required=False,
+#            help='Site to make URL for', choices=all_sites, default=all_sites)
+#    parser.add_argument('remainder', help='other stuff', nargs='*')
+#    # ------------------------------------------------------------------
+#    # ------------------------------------------------------------------
+#    #iogroup = parser.add_argument_group('File I/O')
+#    #iogroup.add_argument('-o', '--output_file', default=None, required=True,
+#    #        help='Output filename', type=str)
+#    #iogroup.add_argument('-R', '--ref_image', default=None, required=True,
+#    #        help='KELT image with WCS')
+#    # ------------------------------------------------------------------
+#    # ------------------------------------------------------------------
+#    ofgroup = parser.add_argument_group('Output format')
+#    fmtparse = ofgroup.add_mutually_exclusive_group()
+#    fmtparse.add_argument('--python', required=False, dest='output_mode',
+#            help='Return Python dictionary with results [default]',
+#            default='pydict', action='store_const', const='pydict')
+#    bash_var = 'ARRAY_NAME'
+#    bash_msg = 'output Bash code snippet (use with eval) to declare '
+#    bash_msg += 'an associative array %s containing results' % bash_var
+#    fmtparse.add_argument('--bash', required=False, default=None,
+#            help=bash_msg, dest='bash_array', metavar=bash_var)
+#    fmtparse.set_defaults(output_mode='pydict')
+#    # ------------------------------------------------------------------
+#    # Miscellany:
+#    miscgroup = parser.add_argument_group('Miscellany')
+#    miscgroup.add_argument('--debug', dest='debug', default=False,
+#            help='Enable extra debugging messages', action='store_true')
+#    miscgroup.add_argument('-q', '--quiet', action='count', default=0,
+#            help='less progress/status reporting')
+#    miscgroup.add_argument('-v', '--verbose', action='count', default=0,
+#            help='more progress/status reporting')
+#    # ------------------------------------------------------------------
+#
+#    context = parser.parse_args()
+#    context.vlevel = 99 if context.debug else (context.verbose-context.quiet)
+#    context.prog_name = prog_name
+#
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+
+
+##--------------------------------------------------------------------------##
+## Where to save results:
+out_dir = 'fix_test'
+if os.path.isdir(out_dir):
+    shutil.rmtree(out_dir)
+os.mkdir(out_dir)
+
+## Images to process:
+img_dir = 'r17577216'
+#img_list = sorted(glob.glob('%s/SPITZER_I1_*_hcfix.fits' % img_dir))
+img_list = sorted(glob.glob('%s/SPITZER_I1_*_clean.fits' % img_dir))
+
+## Stacked frame:
+stack_file = 'zmed_i1_clip30.fits'
+sdata, shdrs = pf.getdata(stack_file, header=True)
+sdata -= np.median(sdata)
+smed, siqrn = rs.calc_ls_med_IQR(sdata)
+
+
+## Select bright pixels:
+s_sigcut =  13.0
+sbright = sdata > s_sigcut * siqrn
+#smasked = sdata.copy()
+#smasked[~bright] = np.nan
+#qsave('lookie.fits', smasked, overwrite=True)
+
+##--------------------------------------------------------------------------##
+
+## Loop over target images and attempt to fix:
+i_sigcut = 75.0
+data_list = []
+hdrs_list = []
+ratio_list = []
+for ipath in img_list:
+    sys.stderr.write("Examining %s ... \n" % ipath)
+    ibase = os.path.basename(ipath)
+    fpath = os.path.join(out_dir, ibase)
+    idata, ihdrs = pf.getdata(ipath, header=True)
+    data_list.append(idata)
+    hdrs_list.append(ihdrs)
+    
+    # identify bright pixels on target frame:
+    ibright = idata > i_sigcut * siqrn
+    rbright = sbright & ~ibright        # safe to scale with
+
+
+    ghost_ratio = idata[rbright] / sdata[rbright]
+    rmed, rsig = rs.calc_ls_med_IQR(ghost_ratio)
+    ratio_list.append(rmed)
+    sys.stderr.write("rmed: %10.5f\n" % rmed)
+    sys.stderr.write("rsig: %10.5f\n" % rsig)
+    sys.stderr.write("\n")
+    ifixed = idata - rmed * sdata
+    qsave(fpath, ifixed)
+
+
+tstamps = astt.Time([h['DATE_OBS'] for h in hdrs_list], scale='utc')
+
+##--------------------------------------------------------------------------##
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+#plt.style.use('bmh')   # Bayesian Methods for Hackers style
+fig_dims = (10, 8)
+fig = plt.figure(1, figsize=fig_dims)
+plt.gcf().clf()
+#fig, axs = plt.subplots(2, 2, sharex=True, figsize=fig_dims, num=1)
+# sharex='col' | sharex='row'
+#fig.frameon = False # disable figure frame drawing
+#fig.subplots_adjust(left=0.07, right=0.95)
+#ax1 = plt.subplot(gs[0, 0])
+ax1 = fig.add_subplot(111)
+#ax1 = fig.add_axes([0, 0, 1, 1])
+#ax1.patch.set_facecolor((0.8, 0.8, 0.8))
+ax1.grid(True)
+#ax1.axis('off')
+
+imtime_sec = 86400.0 * (tstamps.jd - tstamps.jd.min())
+
+ax1.scatter(imtime_sec, ratio_list)
+
+ax1.set_xlabel("Time (sec)")
+ax1.set_ylabel("Ghost ratio (unitless)")
+
+## Disable axis offsets:
+#ax1.xaxis.get_major_formatter().set_useOffset(False)
+#ax1.yaxis.get_major_formatter().set_useOffset(False)
+
+#ax1.plot(kde_pnts, kde_vals)
+
+#blurb = "some text"
+#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes)
+#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes,
+#      va='top', ha='left', bbox=dict(facecolor='white', pad=10.0))
+#      fontdict={'family':'monospace'}) # fixed-width
+
+#colors = cm.rainbow(np.linspace(0, 1, len(plot_list)))
+#for camid, c in zip(plot_list, colors):
+#    cam_data = subsets[camid]
+#    xvalue = cam_data['CCDATEMP']
+#    yvalue = cam_data['PIX_MED']
+#    yvalue = cam_data['IMEAN']
+#    ax1.scatter(xvalue, yvalue, color=c, lw=0, label=camid)
+
+#mtickpos = [2,5,7]
+#ndecades = 1.0   # for symlog, set width of linear portion in units of dex
+#nonposx='mask' | nonposx='clip' | nonposy='mask' | nonposy='clip'
+#ax1.set_xscale('log', basex=10, nonposx='mask', subsx=mtickpos)
+#ax1.set_xscale('log', nonposx='clip', subsx=[3])
+#ax1.set_yscale('symlog', basey=10, linthreshy=0.1, linscaley=ndecades)
+#ax1.xaxis.set_major_formatter(formatter) # re-format x ticks
+#ax1.set_ylim(ax1.get_ylim()[::-1])
+#ax1.set_xlabel('whatever', labelpad=30)  # push X label down 
+
+#ax1.set_xticks([1.0, 3.0, 10.0, 30.0, 100.0])
+#ax1.set_xticks([1, 2, 3], ['Jan', 'Feb', 'Mar'])
+#for label in ax1.get_xticklabels():
+#    label.set_rotation(30)
+#    label.set_fontsize(14) 
+
+#ax1.xaxis.label.set_fontsize(18)
+#ax1.yaxis.label.set_fontsize(18)
+
+#ax1.set_xlim(nice_limits(xvec, pctiles=[1,99], pad=1.2))
+#ax1.set_ylim(nice_limits(yvec, pctiles=[1,99], pad=1.2))
+
+#spts = ax1.scatter(x, y, lw=0, s=5)
+##cbar = fig.colorbar(spts, orientation='vertical')   # old way
+#cbnorm = mplcolors.Normalize(*spts.get_clim())
+#scm = plt.cm.ScalarMappable(norm=cbnorm, cmap=spts.cmap)
+#scm.set_array([])
+#cbar = fig.colorbar(scm, orientation='vertical')
+#cbar = fig.colorbar(scm, ticks=cs.levels, orientation='vertical') # contours
+#cbar.formatter.set_useOffset(False)
+#cbar.update_ticks()
+
+plot_name = 'ratio_decay.png'
+fig.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+plt.draw()
+fig.savefig(plot_name, bbox_inches='tight')
+
+# cyclical colormap ... cmocean.cm.phase
+# cmocean: https://matplotlib.org/cmocean/
+
+
+
+######################################################################
+# CHANGELOG (13_removal_test.py):
+#---------------------------------------------------------------------
+#
+#  2021-02-16:
+#     -- Increased __version__ to 0.0.1.
+#     -- First created 13_removal_test.py.
+#
