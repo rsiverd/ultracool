60daae2ccb6e03434ec90ca566e591aad14c3d67
Rob Siverd <robert.siverd@gmail.com>
2021-02-16 15:27:11 -1000

    initial commit of dither pattern analysis

---
+++ analysis/202102--dither_pattern_ghost/03_illustrate_dithers.py
@@ -0,0 +1,362 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Illustrate dither pattern of imaging sequence.
+#
+# Rob Siverd
+# Created:       2021-02-16
+# Last modified: 2021-02-16
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Logging setup:
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+## Current version:
+__version__ = "0.1.0"
+
+## Optional matplotlib control:
+#from matplotlib import use, rc, rcParams
+#from matplotlib import use
+#from matplotlib import rc
+#from matplotlib import rcParams
+#use('GTKAgg')  # use GTK with Anti-Grain Geometry engine
+#use('agg')     # use Anti-Grain Geometry engine (file only)
+#use('ps')      # use PostScript engine for graphics (file only)
+#use('cairo')   # use Cairo (pretty, file only)
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('font',**{'family':'serif','serif':['Palatino']})
+#rc('font',**{'sans-serif':'Arial','family':'sans-serif'})
+#rc('text', usetex=True) # enables text rendering with LaTeX (slow!)
+#rcParams['axes.formatter.useoffset'] = False   # v. 1.4 and later
+#rcParams['agg.path.chunksize'] = 10000
+#rcParams['font.size'] = 10
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+#import argparse
+#import shutil
+#import resource
+#import signal
+#import glob
+import gc
+import os
+import sys
+import time
+#import vaex
+#import calendar
+#import ephem
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+#import matplotlib.ticker as mt
+#import matplotlib._pylab_helpers as hlp
+#from matplotlib.colors import LogNorm
+import matplotlib.colors as mplcolors
+#import matplotlib.collections as mcoll
+#import matplotlib.gridspec as gridspec
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import PIL.Image as pli
+#import seaborn as sns
+#import cmocean
+#import theil_sen as ts
+#import window_filter as wf
+#import itertools as itt
+_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+##--------------------------------------------------------------------------##
+## Projections with cartopy:
+#try:
+#    import cartopy.crs as ccrs
+#    from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+#    from cartopy.feature.nightshade import Nightshade
+#    #from cartopy import config as cartoconfig
+#except ImportError:
+#    sys.stderr.write("Error: cartopy module not found!\n")
+#    sys.exit(1)
+
+##--------------------------------------------------------------------------##
+
+## Home-brew robust statistics:
+#try:
+#    import robust_stats
+#    reload(robust_stats)
+#    rs = robust_stats
+#except ImportError:
+#    logger.error("module robust_stats not found!  Install and retry.")
+#    sys.stderr.write("\nError!  robust_stats module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Home-brew KDE:
+#try:
+#    import my_kde
+#    reload(my_kde)
+#    mk = my_kde
+#except ImportError:
+#    logger.error("module my_kde not found!  Install and retry.")
+#    sys.stderr.write("\nError!  my_kde module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Fast FITS I/O:
+#try:
+#    import fitsio
+#except ImportError:
+#    logger.error("fitsio module not found!  Install and retry.")
+#    sys.stderr.write("\nError: fitsio module not found!\n")
+#    sys.exit(1)
+
+## Various from astropy:
+try:
+#    import astropy.io.ascii as aia
+    import astropy.io.fits as pf
+#    import astropy.io.votable as av
+#    import astropy.table as apt
+#    import astropy.time as astt
+#    import astropy.wcs as awcs
+#    from astropy import constants as aconst
+#    from astropy import coordinates as coord
+#    from astropy import units as uu
+except ImportError:
+    logger.error("astropy module not found!  Install and retry.")
+#    sys.stderr.write("\nError: astropy module not found!\n")
+    sys.exit(1)
+
+##--------------------------------------------------------------------------##
+## Colors for fancy terminal output:
+NRED    = '\033[0;31m'   ;  BRED    = '\033[1;31m'
+NGREEN  = '\033[0;32m'   ;  BGREEN  = '\033[1;32m'
+NYELLOW = '\033[0;33m'   ;  BYELLOW = '\033[1;33m'
+NBLUE   = '\033[0;34m'   ;  BBLUE   = '\033[1;34m'
+NMAG    = '\033[0;35m'   ;  BMAG    = '\033[1;35m'
+NCYAN   = '\033[0;36m'   ;  BCYAN   = '\033[1;36m'
+NWHITE  = '\033[0;37m'   ;  BWHITE  = '\033[1;37m'
+ENDC    = '\033[0m'
+
+## Suppress colors in cron jobs:
+if (os.getenv('FUNCDEF') == '--nocolors'):
+    NRED    = ''   ;  BRED    = ''
+    NGREEN  = ''   ;  BGREEN  = ''
+    NYELLOW = ''   ;  BYELLOW = ''
+    NBLUE   = ''   ;  BBLUE   = ''
+    NMAG    = ''   ;  BMAG    = ''
+    NCYAN   = ''   ;  BCYAN   = ''
+    NWHITE  = ''   ;  BWHITE  = ''
+    ENDC    = ''
+
+## Fancy text:
+degree_sign = u'\N{DEGREE SIGN}'
+
+## Dividers:
+halfdiv = '-' * 40
+fulldiv = '-' * 80
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (astropy / pyfits):
+#def qsave(iname, idata, header=None, padkeys=1000, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    if header:
+#        while (len(header) < padkeys):
+#            header.append() # pad header
+#    if os.path.isfile(iname):
+#        os.remove(iname)
+#    pf.writeto(iname, idata, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (fitsio):
+#def qsave(iname, idata, header=None, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    #if os.path.isfile(iname):
+#    #    os.remove(iname)
+#    fitsio.write(iname, idata, clobber=True, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+## Header parsing helper:
+def extract_offsets(header):
+    prefixes = ['IMAGE', 'XDIFF', 'YDIFF']
+    values = []
+    for pref in prefixes:
+        keys = [k for k in header.keys() if k.startswith(pref)]
+        values.append(np.array([header[k] for k in keys]))
+    inames, xdiffs, ydiffs = values
+    return inames, -xdiffs, -ydiffs
+
+##--------------------------------------------------------------------------##
+## Folders and files:
+data_dir = '/net/hbfhpc1/bigraid/rsiverd/ucd_targets/2m0415/r17577216'
+ch1_stack_file = '%s/SPITZER_I1_17577216_hcfix_stack.fits' % data_dir
+ch2_stack_file = '%s/SPITZER_I2_17577216_hcfix_stack.fits' % data_dir
+
+## Load stack data and headers:
+ch1_idata, ch1_ihdrs = pf.getdata(ch1_stack_file, header=True)
+ch2_idata, ch2_ihdrs = pf.getdata(ch2_stack_file, header=True)
+
+## Extract image names and offsets:
+ch1_ibase, ch1_xdiff, ch1_ydiff = extract_offsets(ch1_ihdrs)
+ch2_ibase, ch2_xdiff, ch2_ydiff = extract_offsets(ch2_ihdrs)
+
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+#plt.style.use('bmh')   # Bayesian Methods for Hackers style
+fig_dims = (12, 7)
+fig1 = plt.figure(1, figsize=fig_dims)
+fig2 = plt.figure(2, figsize=fig_dims)
+fig1.clf()
+fig2.clf()
+#plt.gcf().clf()
+#fig, axs = plt.subplots(2, 2, sharex=True, figsize=fig_dims, num=1)
+# sharex='col' | sharex='row'
+#fig.frameon = False # disable figure frame drawing
+#fig.subplots_adjust(left=0.07, right=0.85)
+#ax1 = plt.subplot(gs[0, 0])
+ax1 = fig1.add_subplot(111, aspect='equal')
+ax2 = fig2.add_subplot(111, aspect='equal')
+#ax1 = fig.add_axes([0, 0, 1, 1], aspect='equal')
+#ax1 = fig.add_axes([0, 0, 1, 1])
+#ax1.patch.set_facecolor((0.8, 0.8, 0.8))
+ax1.grid(True)
+ax2.grid(True)
+#ax1.axis('off')
+
+## Disable axis offsets:
+#ax1.xaxis.get_major_formatter().set_useOffset(False)
+#ax1.yaxis.get_major_formatter().set_useOffset(False)
+
+#ax1.plot(kde_pnts, kde_vals)
+
+cvec = np.arange(len(ch1_xdiff))
+
+plot_file1 = 'ch1_pattern.png'
+plot_file2 = 'ch2_pattern.png'
+
+
+ax1.plot(ch1_xdiff, ch1_ydiff)
+spts1 = ax1.scatter(ch1_xdiff, ch1_ydiff, c=cvec, s=200)
+ax1.set_xlabel("ch1 X-shifts")
+ax1.set_ylabel("ch1 Y-shifts")
+cbnorm1 = mplcolors.Normalize(*spts1.get_clim())
+scm1 = plt.cm.ScalarMappable(norm=cbnorm1, cmap=spts1.cmap)
+scm1.set_array([])
+cbar1 = fig1.colorbar(scm1, orientation='vertical')
+cbar1.set_label('image number (obs order)')
+fig1.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+plt.draw()
+fig1.savefig(plot_file1)
+
+ax2.plot(ch2_xdiff, ch2_ydiff)
+spts2 = ax2.scatter(ch2_xdiff, ch2_ydiff, c=cvec, s=200)
+ax2.set_xlabel("ch2 X-shifts")
+ax2.set_ylabel("ch2 Y-shifts")
+cbnorm2 = mplcolors.Normalize(*spts2.get_clim())
+scm2 = plt.cm.ScalarMappable(norm=cbnorm2, cmap=spts2.cmap)
+scm2.set_array([])
+cbar2 = fig2.colorbar(scm2, orientation='vertical')
+cbar2.set_label('Image Number (Obs Order)')
+fig2.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+plt.draw()
+fig2.savefig(plot_file2)
+
+#blurb = "some text"
+#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes)
+#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes,
+#      va='top', ha='left', bbox=dict(facecolor='white', pad=10.0))
+#      fontdict={'family':'monospace'}) # fixed-width
+
+#colors = cm.rainbow(np.linspace(0, 1, len(plot_list)))
+#for camid, c in zip(plot_list, colors):
+#    cam_data = subsets[camid]
+#    xvalue = cam_data['CCDATEMP']
+#    yvalue = cam_data['PIX_MED']
+#    yvalue = cam_data['IMEAN']
+#    ax1.scatter(xvalue, yvalue, color=c, lw=0, label=camid)
+
+#mtickpos = [2,5,7]
+#ndecades = 1.0   # for symlog, set width of linear portion in units of dex
+#nonposx='mask' | nonposx='clip' | nonposy='mask' | nonposy='clip'
+#ax1.set_xscale('log', basex=10, nonposx='mask', subsx=mtickpos)
+#ax1.set_xscale('log', nonposx='clip', subsx=[3])
+#ax1.set_yscale('symlog', basey=10, linthreshy=0.1, linscaley=ndecades)
+#ax1.xaxis.set_major_formatter(formatter) # re-format x ticks
+#ax1.set_ylim(ax1.get_ylim()[::-1])
+#ax1.set_xlabel('whatever', labelpad=30)  # push X label down 
+
+#ax1.set_xticks([1.0, 3.0, 10.0, 30.0, 100.0])
+#ax1.set_xticks([1, 2, 3], ['Jan', 'Feb', 'Mar'])
+#for label in ax1.get_xticklabels():
+#    label.set_rotation(30)
+#    label.set_fontsize(14) 
+
+#ax1.xaxis.label.set_fontsize(18)
+#ax1.yaxis.label.set_fontsize(18)
+
+#ax1.set_xlim(nice_limits(xvec, pctiles=[1,99], pad=1.2))
+#ax1.set_ylim(nice_limits(yvec, pctiles=[1,99], pad=1.2))
+
+#spts = ax1.scatter(x, y, lw=0, s=5)
+##cbar = fig.colorbar(spts, orientation='vertical')   # old way
+#cbnorm = mplcolors.Normalize(*spts.get_clim())
+#scm = plt.cm.ScalarMappable(norm=cbnorm, cmap=spts.cmap)
+#scm.set_array([])
+#cbar = fig.colorbar(scm, orientation='vertical')
+#cbar = fig.colorbar(scm, ticks=cs.levels, orientation='vertical') # contours
+#cbar.formatter.set_useOffset(False)
+#cbar.update_ticks()
+
+#fig1.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+#fig2.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+#plt.draw()
+#fig.savefig(plot_name, bbox_inches='tight')
+
+# cyclical colormap ... cmocean.cm.phase
+# cmocean: https://matplotlib.org/cmocean/
+
+
+
+
+######################################################################
+# CHANGELOG (03_illustrate_dithers.py):
+#---------------------------------------------------------------------
+#
+#  2021-02-16:
+#     -- Increased __version__ to 0.0.1.
+#     -- First created 03_illustrate_dithers.py.
+#
