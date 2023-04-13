b8d9feb1a7a240e8360e07b5d99a945180d1cf76
Rob Siverd <robert.siverd@gmail.com>
2023-04-13 16:34:53 -0500

    initial commit of support module for noise model data gathering

---
+++ modules/noisemod_helpers.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Some helper routines for noise model data extraction and organization.
+#
+# Rob Siverd
+# Created:       2023-04-13
+# Last modified: 2023-04-13
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Current version:
+__version__ = "0.1.0"
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+import gc
+import os
+import sys
+import time
+import copy
+#import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import itertools as itt
+#_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+## Because obviously:
+#import warnings
+#if not sys.warnoptions:
+#    warnings.simplefilter("ignore", category=DeprecationWarning)
+#    warnings.simplefilter("ignore", category=UserWarning)
+#    warnings.simplefilter("ignore")
+#    warnings.simplefilter('error')    # halt on warnings
+#with warnings.catch_warnings():
+#    some_risky_activity()
+#with warnings.catch_warnings():
+#    warnings.filterwarnings("ignore", category=DeprecationWarning)
+#    import problem_child1, problem_child2
+
+
+##--------------------------------------------------------------------------##
+
+## Home-brew robust statistics:
+#try:
+#    import robust_stats
+#    reload(robust_stats)
+#    rs = robust_stats
+#except ImportError:
+#    logger.error("module robust_stats not found!  Install and retry.")
+#    sys.stderr.write("\nError!  robust_stats module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Home-brew KDE:
+#try:
+#    import my_kde
+#    reload(my_kde)
+#    mk = my_kde
+#except ImportError:
+#    logger.error("module my_kde not found!  Install and retry.")
+#    sys.stderr.write("\nError!  my_kde module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Various from astropy:
+#try:
+#    import astropy.io.ascii as aia
+#    import astropy.io.fits as pf
+#    import astropy.io.votable as av
+#    import astropy.table as apt
+#    import astropy.time as astt
+#    import astropy.wcs as awcs
+#    from astropy import constants as aconst
+#    from astropy import coordinates as coord
+#    from astropy import units as uu
+#except ImportError:
+#    logger.error("astropy module not found!  Install and retry.")
+#    sys.stderr.write("\nError: astropy module not found!\n")
+#    sys.exit(1)
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (astropy / pyfits):
+#def qsave(iname, idata, header=None, padkeys=1000, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    if header:
+#        while (len(header) < padkeys):
+#            header.append() # pad header
+#    if os.path.isfile(iname):
+#        os.remove(iname)
+#    pf.writeto(iname, idata, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+## Save FITS image with clobber (fitsio):
+#def qsave(iname, idata, header=None, **kwargs):
+#    this_func = sys._getframe().f_code.co_name
+#    parent_func = sys._getframe(1).f_code.co_name
+#    sys.stderr.write("Writing to '%s' ... " % iname)
+#    #if os.path.isfile(iname):
+#    #    os.remove(iname)
+#    fitsio.write(iname, idata, clobber=True, header=header, **kwargs)
+#    sys.stderr.write("done.\n")
+
+##--------------------------------------------------------------------------##
+## Columns to pull from a standardized dataset:
+_noise_model_cols = ['instrument', 'signal', 'ngaia', 'ngaor',
+              'fit_resid_ra_mas', 'fit_resid_de_mas']
+
+##--------------------------------------------------------------------------##
+##------------------     Data Collection/Saving Module      ----------------##
+##--------------------------------------------------------------------------##
+
+class NoiseModHelper(object):
+
+    def __init__(self):
+        self.reset()
+        self._NMC = copy.deepcopy(_noise_model_cols)
+        return
+
+    def reset(self):
+        self.storage = []
+
+    def capture_fit_results(self, data, name):
+        for row in data[data['inliers']]:
+            self.storage.append([name] + [row[x] for x in self._NMC])
+
+    def dump_to_file(self, filename, delim=','):
+        cnames = ['name'] + _noise_model_cols
+        hdrtxt = delim.join(cnames)
+        with open(filename, 'w') as ff:
+            ff.write(hdrtxt + '\n')
+            for stuff in self.storage:
+                ff.write("%s\n" % delim.join([str(x) for x in stuff]))
+                pass
+            pass
+        return
+
+
+######################################################################
+# CHANGELOG (noisemod_helpers.py):
+#---------------------------------------------------------------------
+#
+#  2023-04-13:
+#     -- Increased __version__ to 0.1.0.
+#     -- First created noisemod_helpers.py.
+#
