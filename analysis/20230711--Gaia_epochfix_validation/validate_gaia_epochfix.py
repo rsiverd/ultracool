3979c68fac72ba78875769f5736980f705e58468
Rob Siverd <robert.siverd@gmail.com>
2023-07-24 09:06:15 -1000

    absorbing changes to validation

--- analysis/20230711--Gaia_epochfix_validation/validate_gaia_epochfix.py
+++ analysis/20230711--Gaia_epochfix_validation/validate_gaia_epochfix.py
@@ -43,7 +43,7 @@ def find_gaia_matches(stars, tol_arcsec, ra_col='dra', de_col='dde',
         sxx, syy = target[xx_col], target[yy_col]
         result = gm.nearest_star(sra, sde, tol_deg)
         if result['match']:
-            gcoords = [result['record'][x].values[0] for x in ('ra', 'dec')]
+            gcoords = [result['record'][x].values[0] for x in ('ra', 'dec', 'source_id')]
             matches.append((sxx, syy, sra, sde, *gcoords))
             pass
         pass
@@ -84,15 +84,33 @@ stars = append_fields(stars, 'instmag', imags, usemask=False)
 
 # find matches with reference epoch:
 ref_gaia_matches = find_gaia_matches(stars, 2.0, xx_col='x', yy_col='y')
-ref_sx, ref_sy, ref_sra, ref_sde, ref_gra, ref_gde = zip(*ref_gaia_matches)
+ref_sx, ref_sy, ref_sra, ref_sde, ref_gra, ref_gde, ref_gid = zip(*ref_gaia_matches)
+
+# recover Gaia catalog entries (ref):
+r_hits = gm._srcdata.source_id.isin(ref_gid)
+r_matches = gm._srcdata[r_hits]
+r_speeds = np.hypot(r_matches['pmra'], r_matches['pmdec'])
+#fastest = speeds.argmax()
+r_top_ten_idx = np.argsort(r_speeds)[-10:]
+#fastpar = matches.iloc[fastest]
+r_top_ten_cat = r_matches.iloc[r_top_ten_idx]
 
 # find matches with adjusted epoch:
 sys.stderr.write("Setting epoch ...\n")
 gm.set_epoch(obs_time)
 
 img_gaia_matches = find_gaia_matches(stars, 2.0, xx_col='x', yy_col='y')
-img_sx, img_sy, img_sra, img_sde, img_gra, img_gde = zip(*img_gaia_matches)
+img_sx, img_sy, img_sra, img_sde, img_gra, img_gde, img_gid = zip(*img_gaia_matches)
 
+# recover Gaia catalog entries (ref):
+i_hits = gm._srcdata.source_id.isin(img_gid)
+i_matches = gm._srcdata[i_hits]
+i_speeds = np.hypot(i_matches['pmra'], i_matches['pmdec'])
+#fastest = speeds.argmax()
+i_top_ten_idx = np.argsort(i_speeds)[-10:]
+#fastpar = matches.iloc[fastest]
+i_top_ten_cat = i_matches.iloc[i_top_ten_idx]
+#hits = gm._srcdata.source_id.isin(ref_gid)
 # use dictionaries and sets to identify overlapping matches:
 #img_hits = {}
 #for stuff in img_gaia_matches:
@@ -111,8 +129,8 @@ common_xy = list(set.intersection(ref_xycoo, img_xycoo))
 comm_img_hits = [img_hits[x] for x in common_xy]
 comm_ref_hits = [ref_hits[x] for x in common_xy]
 
-ci_sx, ci_sy, ci_sra, ci_sde, ci_gra, ci_gde = np.array(comm_img_hits).T
-cr_sx, cr_sy, cr_sra, cr_sde, cr_gra, cr_gde = np.array(comm_ref_hits).T
+ci_sx, ci_sy, ci_sra, ci_sde, ci_gra, ci_gde, ci_gid = np.array(comm_img_hits).T
+cr_sx, cr_sy, cr_sra, cr_sde, cr_gra, cr_gde, cr_gid = np.array(comm_ref_hits).T
 
 ci_seps = angle.dAngSep(ci_sra, ci_sde, ci_gra, ci_gde)
 cr_seps = angle.dAngSep(cr_sra, cr_sde, cr_gra, cr_gde)
@@ -138,6 +156,35 @@ sys.stderr.write("\n------------------------------------\n")
 med_ri_improvement = 3600.0 * np.median(cr_seps - ci_seps)
 sys.stderr.write("median improvement (ref_sep - img_sep): %.6f arcsec\n"
         % med_ri_improvement)
+
+
+# -----------------------------------------------------------------------
+# ALTERNATE APPROACH -- dump the top 10 fastest movers to region file
+
+# recover Gaia catalog entries (img):
+
+# -------------
+# epoch shifter:
+def shifted_positions(data, tdiff_yr):
+    cos_dec = np.cos(np.radians(data['dec']))
+
+
+# parameter fetcher:
+def get_4par(target):
+    return target['ra'], target['dec'], 
+
+# region file maker:
+def make_speedy_rfile(rpath, rdata, color):
+    with open(rpath, 'w') as rf:
+        for i,data in rdata.iterrows():
+            rf.write("fk5; annulus(%.5fd, %.5fd, 0.001d, 0.0003d) # color=%s\n"
+                    % (data['ra'], data['dec'], color))
+
+r_rpath = 'fast_movers_ref.reg'
+i_rpath = 'fast_movers_img.reg'
+make_speedy_rfile(r_rpath, r_top_ten_cat, 'red')
+make_speedy_rfile(i_rpath, i_top_ten_cat, 'green')
+
 
 sys.exit(0)
 
