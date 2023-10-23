bfef83271df79f59dabbff61a7abd9dc5075c9ba
Rob Siverd <robert.siverd@gmail.com>
2023-10-23 07:52:55 -1000

    capture uncommitted changes

--- analysis/20230831--CFHT_PSF_variation/01_fetch_cmds.sh
+++ analysis/20230831--CFHT_PSF_variation/01_fetch_cmds.sh
@@ -3,3 +3,5 @@ cp ../../../ucd_cfh_data/for_abby/calib1_p_NE/by_runid/15BQ09/wircam_H2_1838749p
 cp /home/rsiverd/ucd_project/ucd_cfh_data/for_abby/calib1_p_NE/by_runid/12BQ08/wircam_H2_1592961p.fits.fz .
 
 
+cp /home/rsiverd/ucd_project/ucd_cfh_data/for_abby/calib1_p_NE/by_runid/15BQ09/wircam_J_1838731p.fits.fz .
+
