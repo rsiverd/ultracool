d3927602e38b1663baabb9c248ed48e1e56735cb
Rob Siverd <robert.siverd@gmail.com>
2023-10-23 07:51:29 -1000

    initial commit of refraction testing

---
+++ analysis/20230912--atmospheric_refraction_test/02_collect_weather_data.sh
@@ -0,0 +1,12 @@
+
+# First, get a list of all the images:
+imlist="image_paths.txt"
+cfh_dir="/home/rsiverd/ucd_project/ucd_cfh_data/for_abby/calib1_p_NE/by_runid"
+cmde "find $cfh_dir -name 'wircam*.fz' | sort > $imlist"
+
+# Next, gather header data from all of them:
+hdrsave="image_headers.txt"
+want_keys="DATE-OBS UTC-OBS QRUNID"
+want_keys+=" TEMPERAT RELHUMID PRESSURE"  # weather items
+cmde "imhget -l $imlist $want_keys --progress -o $hdrsave"
+
