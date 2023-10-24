fa6eb424df65a804c2e2d75a6f156ab2b8908b4a
Rob Siverd <robert.siverd@gmail.com>
2023-10-24 05:46:52 -1000

    shared config for data prep scripts

---
+++ CFHT/data_prep/config.sh
@@ -0,0 +1,10 @@
+# Some shared configuration items for quick scripts:
+#use_quad="NE"
+#use_quad="NW"
+#use_quad="SE"
+use_quad="SW"
+imlist="image_list.txt"
+targ_dir="calib1_p_${use_quad}"
+key_data="image_hdrs.txt"
+runid_base="${targ_dir}/by_runid"
+
