dd34c17d4e2dede61b8a0c089ac32ff2662f2c6a
Rob Siverd <robert.siverd@gmail.com>
2026-04-02 10:26:43 -0400

    initial commit of diagram code, support module, and current diagram (work in progress)

---
+++ analysis/20260303--CFHT_CDmat_vs_CRPIX/qsquare.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# A fairly simple class for making and tracking vertices of a square.
+#
+# Rob Siverd
+# Created:       2026-03-27
+# Last modified: 2026-03-27
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Current version:
+__version__ = "0.1.0"
+
+## Modules:
+#import glob
+#import io
+#import gc
+import os
+import sys
+import math
+import time
+#import pprint
+#import pickle
+#import vaex
+#import calendar
+#import ephem
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+#import scipy.stats as scst
+#import matplotlib.pyplot as plt
+#import matplotlib.patches as mpatches
+#import matplotlib.cm as cm
+#import matplotlib.ticker as mt
+#import matplotlib._pylab_helpers as hlp
+#from matplotlib.colors import LogNorm
+#import matplotlib.colors as mplcolors
+#import matplotlib.collections as mcoll
+#import matplotlib.gridspec as gridspec
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import PIL.Image as pli
+#import seaborn as sns
+#import cmocean
+#import theil_sen as ts
+#import window_filter as wf
+#import itertools as itt
+_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+##--------------------------------------------------------------------------##
+
+class QSquare(object):
+
+    # Known origins and which vertex they correspond to:
+    _ORIGINS = ['lower-left', 'lower-right',
+                'upper-right', 'upper-left', 'center']
+    _ORI2VTX = {'center'       :  None,
+                'lower-left'   :     0,
+                'lower-right'  :     1,
+                'upper-right'  :     2,
+                'upper-left'   :     3,
+               }
+
+    def __init__(self, diam=1.0, xpos=0.0, ypos=0.0, origin='center',
+                 theta=0.0, vlevel=0, debug=False):
+        #self.orig = 'center'
+        #self._nidx = None
+        #self.diam = diam
+        self._defaults()
+        self.debug = False
+        self.vlevel = vlevel
+        self.set_position(xpos, ypos)
+        self.set_origin(origin)
+        self.set_rotation_rad(theta)
+        #self._update()
+        return
+
+    def _defaults(self):
+        self.diam = 1.0
+        self.orig = 'center'
+        self._nidx = None
+        self.set_position(0.0, 0.0)
+        self.set_rotation_rad(0.)
+
+    # Retrieve the current vertices:
+    def get_vtx(self):
+        self._vtx = self._calc_vtx()
+        return self._vtx
+
+    # Update the origin coordinate chooser.
+    def set_origin(self, origin):
+        #if not origin in self._ORIGINS:
+        if not origin in self._ORI2VTX.keys():
+            sys.stderr.write("Unknown origin: %s\n" % origin)
+            sys.stderr.write("Choices are: %s\n" % str(self._ORI2VTX.keys()))
+            return
+        prev_vtxs = self._calc_vtx()
+        self.orig = origin
+        self._nidx = self._ORI2VTX.get(origin)
+        if self.debug:
+            sys.stderr.write("New origin: %s\n" % self.orig)
+            sys.stderr.write("New  _nidx: %s\n" % self._nidx)
+        # nudge position to compensate for any origin change:
+        curr_vtxs = self._calc_vtx()
+        dx, dy = curr_vtxs[:, 0] - prev_vtxs[:, 0]
+        self.set_position_rel(-dx, -dy)
+
+
+    # Update the X,Y position:
+    def set_position(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.posn = np.array([xpos, ypos])
+    def set_position_rel(self, rel_xpos, rel_ypos):
+        self.xpos = self.xpos + rel_xpos
+        self.ypos = self.ypos + rel_ypos
+        self.posn = np.array([self.xpos, self.ypos])
+
+    # Update rotation angle:
+    def set_rotation_rad(self, r_theta):
+        self.theta = r_theta
+        self._rmat = self._vector_zrot(r_theta)
+    def set_rotation_deg(self, d_theta):
+        return self.set_rotation_rad(np.radians(d_theta))
+
+    # Make 0-center vertices:
+    def _mkvtx0(self):
+        LL = BB = -0.5 * self.diam      # left, bottom
+        RR = TT =  0.5 * self.diam      # right, top
+        sqvtx = np.array([(LL, BB),     # lower-left
+                          (RR, BB),     # lower-right
+                          (RR, TT),     # upper-right
+                          (LL, TT),     # upper-left
+                          (LL, BB)])    # lower-left (closure)
+        return sqvtx.T.copy()
+
+    # Recalculate the square
+    def _calc_vtx(self):
+        vtx = self._mkvtx0()            # make 0-centered square
+        if self._nidx is not None:      # compensate origin
+            vtx -= vtx[:, self._nidx][:, None]
+        vtx = self._rmat @ vtx
+        return vtx + self.posn[:, None]
+
+    # Z-axis rotation matrix:
+    @staticmethod
+    def _vector_zrot(r_theta):
+        c, s = math.cos(r_theta), math.sin(r_theta)
+        return np.array((c, -s, s, c)).reshape(2, 2)
+
+######################################################################
+# CHANGELOG (qsquare.py):
+#---------------------------------------------------------------------
+#
+#  2026-03-27:
+#     -- Increased __version__ to 0.1.0.
+#     -- First created qsquare.py.
+#
---
+++ analysis/20260303--CFHT_CDmat_vs_CRPIX/transform_diagrams.py
@@ -0,0 +1,520 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 et tw=80 :
+#
+# Produce diagrams to illustrate sensor-to-sensor transform
+# geometry.
+#
+# Rob Siverd
+# Created:       2026-03-27
+# Last modified: 2026-03-27
+#--------------------------------------------------------------------------
+#**************************************************************************
+#--------------------------------------------------------------------------
+
+## Current version:
+__version__ = "0.1.0"
+
+## Optional matplotlib control:
+#from matplotlib import use, rc, rcParams
+#from matplotlib import use
+#from matplotlib import rc
+#from matplotlib import rcParams
+#use('GTKAgg')  # use GTK with Anti-Grain Geometry engine
+#use('agg')     # use Anti-Grain Geometry engine (file only)
+#use('ps')      # use PostScript engine for graphics (file only)
+#use('cairo')   # use Cairo (pretty, file only)
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+#rc('font',**{'family':'serif','serif':['Palatino']})
+#rc('font',**{'sans-serif':'Arial','family':'sans-serif'})
+#rc('text', usetex=True) # enables text rendering with LaTeX (slow!)
+#rcParams['axes.formatter.useoffset'] = False   # v. 1.4 and later
+#rcParams['agg.path.chunksize'] = 10000
+#rcParams['font.size'] = 10
+
+## Python version-agnostic module reloading:
+try:
+    reload                              # Python 2.7
+except NameError:
+    try:
+        from importlib import reload    # Python 3.4+
+    except ImportError:
+        from imp import reload          # Python 3.0 - 3.3
+
+## Modules:
+#import argparse
+#import shutil
+#import resource
+#import signal
+#import glob
+#import io
+import gc
+import os
+import sys
+import time
+#import pprint
+#import pickle
+#import vaex
+#import calendar
+#import ephem
+import numpy as np
+#from numpy.lib.recfunctions import append_fields
+#import datetime as dt
+#from dateutil import parser as dtp
+#import scipy.linalg as sla
+#import scipy.signal as ssig
+#import scipy.ndimage as ndi
+#import scipy.optimize as opti
+#import scipy.interpolate as stp
+#import scipy.spatial.distance as ssd
+#import scipy.stats as scst
+import matplotlib.pyplot as plt
+#import matplotlib.patches as mpatches
+#import matplotlib.cm as cm
+#import matplotlib.ticker as mt
+#import matplotlib._pylab_helpers as hlp
+#from matplotlib.colors import LogNorm
+#import matplotlib.colors as mplcolors
+#import matplotlib.collections as mcoll
+#import matplotlib.gridspec as gridspec
+#from functools import partial
+#from collections import OrderedDict
+#from collections.abc import Iterable
+#import multiprocessing as mp
+#np.set_printoptions(suppress=True, linewidth=160)
+#import pandas as pd
+#import statsmodels.api as sm
+#import statsmodels.formula.api as smf
+#from statsmodels.regression.quantile_regression import QuantReg
+#import theil_sen as ts
+#import window_filter as wf
+#import itertools as itt
+_have_np_vers = float('.'.join(np.__version__.split('.')[:2]))
+
+## Square helper:
+import qsquare
+reload(qsquare)
+
+##--------------------------------------------------------------------------##
+## Disable buffering on stdout/stderr:
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
+sys.stderr = Unbuffered(sys.stderr)
+
+##--------------------------------------------------------------------------##
+
+## Pickle store routine:
+def stash_as_pickle(filename, thing):
+    with open(filename, 'wb') as sapf:
+        pickle.dump(thing, sapf)
+    return
+
+## Pickle load routine:
+def load_pickled_object(filename):
+    with open(filename, 'rb') as lpof:
+        thing = pickle.load(lpof)
+    return thing
+
+##--------------------------------------------------------------------------##
+
+## Home-brew robust statistics:
+#try:
+#    import robust_stats
+#    reload(robust_stats)
+#    rs = robust_stats
+#except ImportError:
+#    logger.error("module robust_stats not found!  Install and retry.")
+#    sys.stderr.write("\nError!  robust_stats module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Home-brew KDE:
+#try:
+#    import my_kde
+#    reload(my_kde)
+#    mk = my_kde
+#except ImportError:
+#    logger.error("module my_kde not found!  Install and retry.")
+#    sys.stderr.write("\nError!  my_kde module not found!\n"
+#           "Please install and try again ...\n\n")
+#    sys.exit(1)
+
+## Various from astropy:
+#try:
+#    import astropy.io.ascii as aia
+#    import astropy.io.fits as pf
+#    import astropy.io.votable as av
+#    import astropy.table as apt
+#    import astropy.time as astt
+#    import astropy.wcs as awcs
+#    from astropy import constants as aconst
+#    from astropy import coordinates as coord
+#    from astropy import units as uu
+#except ImportError:
+#    logger.error("astropy module not found!  Install and retry.")
+#    sys.stderr.write("\nError: astropy module not found!\n")
+#    sys.exit(1)
+
+##--------------------------------------------------------------------------##
+## Colors for fancy terminal output:
+NRED    = '\033[0;31m'   ;  BRED    = '\033[1;31m'
+NGREEN  = '\033[0;32m'   ;  BGREEN  = '\033[1;32m'
+NYELLOW = '\033[0;33m'   ;  BYELLOW = '\033[1;33m'
+NBLUE   = '\033[0;34m'   ;  BBLUE   = '\033[1;34m'
+NMAG    = '\033[0;35m'   ;  BMAG    = '\033[1;35m'
+NCYAN   = '\033[0;36m'   ;  BCYAN   = '\033[1;36m'
+NWHITE  = '\033[0;37m'   ;  BWHITE  = '\033[1;37m'
+ENDC    = '\033[0m'
+
+## Suppress colors in cron jobs:
+if (os.getenv('FUNCDEF') == '--nocolors'):
+    NRED    = ''   ;  BRED    = ''
+    NGREEN  = ''   ;  BGREEN  = ''
+    NYELLOW = ''   ;  BYELLOW = ''
+    NBLUE   = ''   ;  BBLUE   = ''
+    NMAG    = ''   ;  BMAG    = ''
+    NCYAN   = ''   ;  BCYAN   = ''
+    NWHITE  = ''   ;  BWHITE  = ''
+    ENDC    = ''
+
+## Fancy text:
+degree_sign = u'\N{DEGREE SIGN}'
+
+## Dividers:
+halfdiv = '-' * 40
+fulldiv = '-' * 80
+
+##--------------------------------------------------------------------------##
+def ldmap(things):
+    return dict(zip(things, range(len(things))))
+
+def argnear(vec, val):
+    return (np.abs(vec - val)).argmin()
+
+
+
+
+##--------------------------------------------------------------------------##
+##------------------         Parse Command Line             ----------------##
+##--------------------------------------------------------------------------##
+
+## Parse arguments and run script:
+#class MyParser(argparse.ArgumentParser):
+#    def error(self, message):
+#        sys.stderr.write('error: %s\n' % message)
+#        self.print_help()
+#        sys.exit(2)
+#
+### Enable raw text AND display of defaults:
+#class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+#                        argparse.RawDescriptionHelpFormatter):
+#    pass
+#
+### Parse the command line:
+#if __name__ == '__main__':
+#
+#    # ------------------------------------------------------------------
+#    prog_name = os.path.basename(__file__)
+#    descr_txt = """
+#    PUT DESCRIPTION HERE.
+#    
+#    Version: %s
+#    """ % __version__
+#    parser = argparse.ArgumentParser(
+#            prog='PROGRAM_NAME_HERE',
+#            prog=os.path.basename(__file__),
+#            #formatter_class=argparse.RawTextHelpFormatter)
+#            description='PUT DESCRIPTION HERE.')
+#            #description=descr_txt)
+#    parser = MyParser(prog=prog_name, description=descr_txt)
+#                          #formatter_class=argparse.RawTextHelpFormatter)
+#    # ------------------------------------------------------------------
+#    parser.set_defaults(thing1='value1', thing2='value2')
+#    # ------------------------------------------------------------------
+#    parser.add_argument('firstpos', help='first positional argument')
+#    parser.add_argument('-w', '--whatever', required=False, default=5.0,
+#            help='some option with default [def: %(default)s]', type=float)
+#    parser.add_argument('-s', '--site',
+#            help='Site to retrieve data for', required=True)
+#    parser.add_argument('-n', '--number_of_days', default=1,
+#            help='Number of days of data to retrieve.')
+#    parser.add_argument('-o', '--output_file', 
+#            default='observations.csv', help='Output filename.')
+#    parser.add_argument('--start', type=str, default=None, 
+#            help="Start time for date range query.")
+#    parser.add_argument('--end', type=str, default=None,
+#            help="End time for date range query.")
+#    parser.add_argument('-d', '--dayshift', required=False, default=0,
+#            help='Switch between days (1=tom, 0=today, -1=yest', type=int)
+#    parser.add_argument('-e', '--encl', nargs=1, required=False,
+#            help='Encl to make URL for', choices=all_encls, default=all_encls)
+#    parser.add_argument('-s', '--site', nargs=1, required=False,
+#            help='Site to make URL for', choices=all_sites, default=all_sites)
+#    parser.add_argument('remainder', help='other stuff', nargs='*')
+#    # ------------------------------------------------------------------
+#    # ------------------------------------------------------------------
+#    #iogroup = parser.add_argument_group('File I/O')
+#    #iogroup.add_argument('-o', '--output_file', default=None, required=True,
+#    #        help='Output filename', type=str)
+#    #iogroup.add_argument('-R', '--ref_image', default=None, required=True,
+#    #        help='KELT image with WCS')
+#    # ------------------------------------------------------------------
+#    # ------------------------------------------------------------------
+#    ofgroup = parser.add_argument_group('Output format')
+#    fmtparse = ofgroup.add_mutually_exclusive_group()
+#    fmtparse.add_argument('--python', required=False, dest='output_mode',
+#            help='Return Python dictionary with results [default]',
+#            default='pydict', action='store_const', const='pydict')
+#    bash_var = 'ARRAY_NAME'
+#    bash_msg = 'output Bash code snippet (use with eval) to declare '
+#    bash_msg += 'an associative array %s containing results' % bash_var
+#    fmtparse.add_argument('--bash', required=False, default=None,
+#            help=bash_msg, dest='bash_array', metavar=bash_var)
+#    fmtparse.set_defaults(output_mode='pydict')
+#    # ------------------------------------------------------------------
+#    # Miscellany:
+#    miscgroup = parser.add_argument_group('Miscellany')
+#    miscgroup.add_argument('--debug', dest='debug', default=False,
+#            help='Enable extra debugging messages', action='store_true')
+#    miscgroup.add_argument('-q', '--quiet', action='count', default=0,
+#            help='less progress/status reporting')
+#    miscgroup.add_argument('-v', '--verbose', action='count', default=0,
+#            help='more progress/status reporting')
+#    # ------------------------------------------------------------------
+#
+#    context = parser.parse_args()
+#    context.vlevel = 99 if context.debug else (context.verbose-context.quiet)
+#    context.prog_name = prog_name
+#
+##--------------------------------------------------------------------------##
+
+##--------------------------------------------------------------------------##
+## New-style string formatting (more at https://pyformat.info/):
+
+#oldway = '%s %s' % ('one', 'two')
+#newway = '{} {}'.format('one', 'two')
+
+#oldway = '%d %d' % (1, 2)
+#newway = '{} {}'.format(1, 2)
+
+# Centered (new-only):
+#newctr = '{:^10}'.format('test')      # prints "   test   "
+
+# Numbers:
+#oldway = '%06.2f' % (3.141592653589793,)
+#newway = '{:06.2f}'.format(3.141592653589793)
+
+##--------------------------------------------------------------------------##
+## Simple class to track and manipulate the vertices of a square:
+#class qsquare(object):
+#    def __init__(self, diam=1.0, xcenter=0.0, ycenter=0.0):
+#        self.diam = diam
+#        self.xcenter = xcenter
+#        self.ycenter = ycenter
+#        self._update()
+#        return
+#
+#    # 
+#    def _update(self, diam):
+#        LL = BB = -0.5 * self.diam              # left, bottom
+#        RR = TT =  0.5 * self.diam              # right, top
+#        square_vtx = np.array([(LL, BB),        # lower-left
+#                               (RR, BB),        # lower-right
+#                               (RR, TT),        # upper-right
+#                               (LL, TT),        # upper-left
+#                               (LL, BB)])       # lower-left 
+#        
+
+#store keep track of square vertices and 
+
+##--------------------------------------------------------------------------##
+## Define a closed unit square centered at X=0,Y=0 by its corners. This will
+## serve as the sensor outline throughout the diagram.
+diam = 1.0
+LL = BB = -0.5 * diam     # left, bottom
+RR = TT =  0.5 * diam     # right, top
+square_vtx = np.array([(LL, BB),      # lower-left
+                       (RR, BB),      # lower-right
+                       (RR, TT),      # upper-right
+                       (LL, TT),      # upper-left
+                       (LL, BB)])     # lower-left 
+#square_vtx = square_vtx.T
+
+ne_sensor = qsquare.QSquare()
+nw_sensor = qsquare.QSquare()
+
+#print("BEFORE: %s" % ne_sensor.get_vtx())
+ne_sensor.set_origin('lower-left')
+nw_sensor.set_origin('lower-left')
+#print("AFTER:  %s" % ne_sensor.get_vtx())
+#print(ne_sensor.get_vtx())
+
+#Rz = ne_sensor._vector_zrot(r_theta)
+#vtx = ne_sensor._update()
+#ne_sensor.set_rotation_rad(r_theta)
+#print(ne_sensor.get_vtx())
+#print(nw_sensor.get_vtx())
+
+gutter_frac = 0.25
+qq_xshift = 0.5 * diam * (1.0 + gutter_frac)
+ne_sensor.set_position_rel(-qq_xshift, 0.0)
+nw_sensor.set_position_rel(+qq_xshift, 0.0)
+#ne_sensor = square_vtx - qq_xshift
+#nw_sensor = square_vtx + qq_xshift
+#print(ne_sensor.get_vtx())
+
+## Get NW coords before/after rotation:
+nw_par_vtx = nw_sensor.get_vtx()
+r_theta = np.radians(45.0)
+r_theta = np.radians(10.0)
+#r_theta = np.radians( 5.0)
+nw_sensor.set_rotation_rad(r_theta)
+nw_rot_vtx = nw_sensor.get_vtx()
+
+##--------------------------------------------------------------------------##
+## Plot config:
+
+# gridspec examples:
+# https://matplotlib.org/users/gridspec.html
+
+#gs1 = gridspec.GridSpec(4, 4)
+#gs1.update(wspace=0.025, hspace=0.05)  # set axis spacing
+
+#ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=3) # top-left + center + right
+#ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=2) # mid-left + mid-center
+#ax3 = plt.subplot2grid((3, 3), (1, 2), rowspan=2) # mid-right + bot-right
+#ax4 = plt.subplot2grid((3, 3), (2, 0))            # bot-left
+#ax5 = plt.subplot2grid((3, 3), (2, 1))            # bot-center
+
+
+#sys.exit(0)
+
+##--------------------------------------------------------------------------##
+#plt.style.use('bmh')   # Bayesian Methods for Hackers style
+fig_dims = (13, 7)
+fig = plt.figure(1, figsize=fig_dims)
+plt.gcf().clf()
+#fig, axs = plt.subplots(nrows=2, ncols=2, num=1, clear=True, figsize=fig_dims,
+#                        sharex=True, squeeze=False)
+# sharex='col' | sharex='row' | squeeze=False
+#fig.frameon = False # disable figure frame drawing
+#fig.subplots_adjust(left=0.07, right=0.95)
+#ax1 = plt.subplot(gs[0, 0])
+ax1 = fig.add_subplot(111, aspect='equal')
+#ax1 = fig.add_subplot(111, polar=True)
+#ax1 = fig.add_axes([0, 0, 1, 1])
+ax1.patch.set_facecolor((0.8, 0.8, 0.8))
+ax1.grid(True)
+#ax1.axis('off')
+
+# Disable tick labels:
+ax1.set_xticklabels([])
+ax1.set_yticklabels([])
+
+
+# CCD boundary colors:
+#ne_par_color = '
+nw_par_color = 'orange'
+nw_rot_color = 'g'
+
+ax1.plot(*ne_sensor.get_vtx(), label='NE')
+#ax1.plot(*nw_sensor.get_vtx(), label='NW')
+ax1.plot(*nw_par_vtx, color=nw_par_color, label='NW (par)')
+ax1.plot(*nw_rot_vtx, color=nw_rot_color, ls='--', label='NW (rot)')
+#ax1.plot(*nw_sensor)
+ax1.legend(loc='best')
+
+## Polar scatter:
+#skw = {'lw':0, 's':15}
+#ax1.scatter(azm_rad, zdist_deg, **skw)
+
+## For polar axes:
+#ax1.set_rmin( 0.0)                  # if using altitude in degrees
+#ax1.set_rmax(90.0)                  # if using altitude in degrees
+#ax1.set_theta_direction(-1)         # clockwise
+#ax1.set_theta_direction(+1)         # counterclockwise
+#ax1.set_theta_zero_location("N")    # North-up
+#ax1.set_rlabel_position(-30.0)      # move labels 30 degrees
+
+## Disable axis offsets:
+#ax1.xaxis.get_major_formatter().set_useOffset(False)
+#ax1.yaxis.get_major_formatter().set_useOffset(False)
+
+#ax1.plot(kde_pnts, kde_vals)
+
+#ax1.pcolormesh(xx, yy, ivals)
+
+#blurb = "some text"
+#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes)
+#ax1.text(0.5, 0.5, blurb, transform=ax1.transAxes,
+#      va='top', ha='left', bbox=dict(facecolor='white', pad=10.0))
+#      fontdict={'family':'monospace'}) # fixed-width
+#      fontdict={'fontsize':24}) # larger typeface
+
+#colors = cm.rainbow(np.linspace(0, 1, len(plot_list)))
+#for camid, c in zip(plot_list, colors):
+#    cam_data = subsets[camid]
+#    xvalue = cam_data['CCDATEMP']
+#    yvalue = cam_data['PIX_MED']
+#    yvalue = cam_data['IMEAN']
+#    ax1.scatter(xvalue, yvalue, color=c, lw=0, label=camid)
+
+#mtickpos = [2,5,7]
+#ndecades = 1.0   # for symlog, set width of linear portion in units of dex
+#nonposx='mask' | nonposx='clip' | nonposy='mask' | nonposy='clip'
+#ax1.set_xscale('log', basex=10, nonposx='mask', subsx=mtickpos)
+#ax1.set_xscale('log', nonposx='clip', subsx=[3])
+#ax1.set_yscale('symlog', basey=10, linthreshy=0.1, linscaley=ndecades)
+#ax1.xaxis.set_major_formatter(fptformat) # re-format x ticks
+#ax1.set_ylim(ax1.get_ylim()[::-1])
+#ax1.set_xlabel('whatever', labelpad=30)  # push X label down 
+
+#ax1.set_xticks([1.0, 3.0, 10.0, 30.0, 100.0])
+#ax1.xticks([1, 2, 3], ['Jan', 'Feb', 'Mar'])
+#ax1.xticks([1, 2, 3], ['Jan', 'Feb', 'Mar'], rotation=45)
+#for label in ax1.get_xticklabels():
+#    label.set_rotation(30)
+#    label.set_fontsize(14) 
+
+#ax1.xaxis.label.set_fontsize(18)
+#ax1.yaxis.label.set_fontsize(18)
+
+#ax1.set_xlim(nice_limits(xvec, pctiles=[1,99], pad=1.2))
+#ax1.set_ylim(nice_limits(yvec, pctiles=[1,99], pad=1.2))
+
+#ax1.legend(loc='best', prop={'size':24})
+
+#spts = ax1.scatter(x, y, lw=0, s=5)
+##cbar = fig.colorbar(spts, orientation='vertical')   # old way
+#cbnorm = mplcolors.Normalize(*spts.get_clim())
+#scm = plt.cm.ScalarMappable(norm=cbnorm, cmap=spts.cmap)
+#scm.set_array([])
+#cbar = fig.colorbar(scm, orientation='vertical')
+#cbar = fig.colorbar(scm, ticks=cs.levels, orientation='vertical') # contours
+#cbar.formatter.set_useOffset(False)
+#cbar.update_ticks()
+
+#fig.align_labels()
+#fig.align_xlabels()
+#fig.align_ylabels()
+fig.tight_layout() # adjust boundaries sensibly, matplotlib v1.1+
+plt.draw()
+plot_name = 'sensors_diagram.png'
+fig.savefig(plot_name, bbox_inches='tight')
+
+
+######################################################################
+# CHANGELOG (transform_diagrams.py):
+#---------------------------------------------------------------------
+#
+#  2026-03-27:
+#     -- Increased __version__ to 0.1.0.
+#     -- First created transform_diagrams.py.
+#
